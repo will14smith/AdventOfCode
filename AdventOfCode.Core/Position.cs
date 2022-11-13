c59e0dc9754542016068da7b8f98d8d71371a0b1
Will Smith <will@toxon.co.uk>
2022-11-13 16:02:36 +0000

    Solve Day18

---
+++ AdventOfCode.Core/Position.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Core;
+
+public record Position(int X, int Y)
+{
+    public static readonly Position Identity = new(0, 0);
+    public static Position operator +(Position a, Position b) => new(a.X + b.X, a.Y + b.Y);
+
+    public IEnumerable<Position> Neighbours
+    {
+        get
+        {
+            for(var y = -1; y <= 1; y++)
+            for(var x = -1; x <= 1; x++)
+                yield return new Position(X + x, Y + y);
+        }
+    }
+    public IEnumerable<Position> StrictNeighbours
+    {
+        get
+        {
+            for(var y = -1; y <= 1; y++)
+            for(var x = -1; x <= 1; x++)
+                if(x != 0 || y != 0)
+                    yield return new Position(X + x, Y + y);
+        }
+    }
+    public IEnumerable<Position> OrthogonalNeighbours
+    {
+        get
+        {
+            yield return this with { X = X + 1 };
+            yield return this with { X = X - 1 };
+            yield return this with { Y = Y + 1 };
+            yield return this with { Y = Y - 1 };
+        }
+    }
+}
---
+++ AdventOfCode2015/Day18.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode2015;
+
+[Day]
+public partial class Day18 : Day<Grid<bool>, int, int>
+{
+    protected override Grid<bool> Parse(string input) => GridParser.ParseBool(input, '#');
+
+    protected override int Part1(Grid<bool> input)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            input = Apply(input);
+        }
+
+        return input.Count(x => x);
+    }
+    
+    protected override int Part2(Grid<bool> input)
+    {
+        input = Broken(input);
+        
+        for (int i = 0; i < 100; i++)
+        {
+            input = Apply(input);
+            input = Broken(input);
+        }
+
+        return input.Count(x => x);
+    }
+    
+    private static Grid<bool> Apply(Grid<bool> grid)
+    {
+        return grid.Select(static (cell, grid, position) =>
+        {
+            var neighbours = position.StrictNeighbours.Where(grid.IsValid).Select(grid.Get).Count(x => x);
+            return cell ? neighbours is 2 or 3 : neighbours is 3;
+        });
+    }
+    
+    private static Grid<bool> Broken(Grid<bool> grid)
+    {
+        grid[0, 0] = true;
+        grid[grid.Width - 1, 0] = true;
+        grid[0, grid.Height - 1] = true;
+        grid[grid.Width - 1, grid.Height - 1] = true;
+
+        return grid;
+    }
+}
