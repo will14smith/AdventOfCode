96b8ea96a2d85cb7a2fdb0606d7300d2c4cf3874
Will Smith <will@toxon.co.uk>
2022-11-20 20:35:10 +0000

    Solve Day03

--- AdventOfCode2016/Day03.cs
+++ AdventOfCode2016/Day03.cs
@@ -1,12 +1,58 @@
 namespace AdventOfCode2016;
 
 [Day]
-public partial class Day03 : Day<Day03.Model, int, int>
+public partial class Day03 : LineDay<Day03.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    protected override Model ParseLine(string input)
+    {
+        var parts = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        return new Model(parts[0], parts[1], parts[2]);
+    }
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample("5 10 25", 0)]
+    [Sample("5 10 11", 1)]
+    protected override int Part1(IEnumerable<Model> input) => CountValid(input);
+    
+    protected override int Part2(IEnumerable<Model> input) => CountValid(Remap(input));
 
-    public record Model;
+    private static IEnumerable<Model> Remap(IEnumerable<Model> input)
+    {
+        var array = input.ToArray();
+        var newArray = new Model[array.Length];
+        
+        for (var i = 0; i < array.Length; i += 3)
+        {
+            newArray[i] = new Model(array[i].A, array[i + 1].A, array[i + 2].A);
+            newArray[i+1] = new Model(array[i].B, array[i + 1].B, array[i + 2].B);
+            newArray[i+2] = new Model(array[i].C, array[i + 1].C, array[i + 2].C);
+        }
+
+        return newArray;
+    }
+
+    private static int CountValid(IEnumerable<Model> input)
+    {
+        return input.Select(x => x.Ordered).Count(x => x.X + x.Y > x.Z);
+    }
+
+
+    public record Model(int A, int B, int C)
+    {
+        public (int X, int Y, int Z) Ordered
+        {
+            get
+            {
+                var x = A;
+                var y = B;
+                var z = C;
+
+                if (x > y) (x, y) = (y, x);
+                if (y > z) (y, z) = (z, y);
+                if (x > y) (x, y) = (y, x);
+                
+                return (x, y, z);
+            }
+        }
+            
+    }
 }
