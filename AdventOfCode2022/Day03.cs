e3c078376392068ba0635f3b05c74e32893949f2
Will Smith <will@toxon.co.uk>
2022-12-03 08:12:08 +0000

    Solve Day03

--- AdventOfCode2022/Day03.cs
+++ AdventOfCode2022/Day03.cs
@@ -1,12 +1,45 @@
 namespace AdventOfCode2022;
 
 [Day]
-public partial class Day03 : Day<Day03.Model, int, int>
+public partial class Day03 : LineDay<Day03.Bag, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    private const string Sample = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+    
+    protected override Bag ParseLine(string input)
+    {
+        var halfLen = input.Length >> 1;
+        return new Bag(input[..halfLen].ToCharArray(), input[halfLen..].ToCharArray());
+    }
+    
+    [Sample(Sample, 157)]
+    protected override int Part1(IEnumerable<Bag> input) => input.Sum(x => x.CommonItems.Sum(Score));
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample(Sample, 70)]
+    protected override int Part2(IEnumerable<Bag> input) => GroupIntoThrees(input).Select(x => x.A.All.Intersect(x.B.All).Intersect(x.C.All).Sum(Score)).Sum();
 
-    public record Model;
+    private static IEnumerable<(Bag A, Bag B, Bag C)> GroupIntoThrees(IEnumerable<Bag> input)
+    {
+        var inputList = input.ToArray();
+
+        for (var i = 0; i < inputList.Length; i+=3)
+        {
+            yield return (inputList[i], inputList[i + 1], inputList[i + 2]);
+        }
+    }
+
+    private static int Score(char item)
+    {
+        return item switch
+        {
+            >= 'a' and <= 'z' => item - 'a' + 1,
+            >= 'A' and <= 'Z' => item - 'A' + 27,
+            _ => throw new ArgumentOutOfRangeException(nameof(item))
+        };
+    }
+    
+    public record Bag(IReadOnlyList<char> First, IReadOnlyList<char> Second)
+    {
+        public IReadOnlySet<char> CommonItems => First.Intersect(Second).ToHashSet();
+        public IReadOnlySet<char> All => First.Concat(Second).ToHashSet();
+    }
 }
