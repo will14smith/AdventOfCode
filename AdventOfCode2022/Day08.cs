548bb5d9da4b350c4bbde6dfd7dcad58b51d71a9
Will Smith <will@toxon.co.uk>
2022-12-08 08:40:17 +0000

    Refactor Day08

--- AdventOfCode2022/Day08.cs
+++ AdventOfCode2022/Day08.cs
@@ -8,81 +8,84 @@ public partial class Day08 : Day<Day08.Model, int, int>
     protected override Model Parse(string input) => new(GridParser.ParseInt(input));
 
     [Sample(Sample, 21)]
-    protected override int Part1(Model input) => CountVisibleOutside(input.Trees);
+    protected override int Part1(Model input) => CountVisibleFromOutside(input.Trees);
     [Sample(Sample, 8)]
-    protected override int Part2(Model input) => CountVisibleFrom(input.Trees);
-
-    private int CountVisibleFrom(Grid<int> trees) => trees.Keys().Max(x => CountVisibleFrom(trees, x));
-
-    private int CountVisibleFrom(Grid<int> trees, Position centre)
+    protected override int Part2(Model input) => FindMaxScenic(input.Trees);
+    
+    private static int CountVisibleFromOutside(Grid<int> trees)
     {
-        var score = 1;
-        var deltas = new[] { new Position(0, 1), new Position(0, -1), new Position(1, 0), new Position(-1, 0) };
-        
-        var centreTree = trees[centre];
-
-        foreach (var delta in deltas)
-        {
-            var count = 0;
-            
-            var position = centre + delta;
-            while (trees.IsValid(position))
-            {
-                var tree = trees[position];
-                
-                position += delta;
-                count++;
-                
-                if (tree >= centreTree)
-                {
-                    break;
-                }
-            }
-
-            score *= count;
-        }
-
-        return score;
-    }
-
-    private int CountVisibleOutside(Grid<int> trees)
-    {
-        var startPoints = new List<(Position Position, Position Delta)>();
-        for (int x = 0; x < trees.Width; x++)
-        {
-            startPoints.Add((new Position(x, 0), new Position(0, 1)));
-            startPoints.Add((new Position(x, trees.Height - 1), new Position(0, -1)));
-        }
-        for (int y = 0; y < trees.Height; y++)
-        {
-            startPoints.Add((new Position(0, y), new Position(1, 0)));
-            startPoints.Add((new Position(trees.Width - 1, y), new Position(-1, 0)));
-        }
-
-        return startPoints.SelectMany(x => FindVisibleOutside(trees, x.Position, x.Delta)).Distinct().Count();
-    }
-
-    private IReadOnlySet<Position> FindVisibleOutside(Grid<int> trees, Position start, Position delta)
-    {
-        var position = start;
-
-        var previous = -1;
         var visible = new HashSet<Position>();
+
+        for (var x = 0; x < trees.Width; x++)
+        {
+            visible.UnionWith(FindVisibleInSpan(trees, GetSpanWithX(x, 0, trees.Height - 1)));
+            visible.UnionWith(FindVisibleInSpan(trees, GetSpanWithX(x, 0, trees.Height - 1).Reverse()));
+        }
         
-        while (trees.IsValid(position))
+        for (var y = 0; y < trees.Height; y++)
+        {
+            visible.UnionWith(FindVisibleInSpan(trees, GetSpanWithY(y, 0, trees.Width - 1)));
+            visible.UnionWith(FindVisibleInSpan(trees, GetSpanWithY(y, 0, trees.Width - 1).Reverse()));
+        }
+
+        return visible.Count;
+    }
+
+    private static IEnumerable<Position> FindVisibleInSpan(Grid<int> trees, IEnumerable<Position> span)
+    {
+        var height = -1;
+        foreach (var position in span)
         {
             var tree = trees[position];
-            if (tree > previous)
+            if (tree <= height)
             {
-                previous = tree;
-                visible.Add(position);
+                continue;
             }
             
-            position += delta;
+            yield return position;
+            height = tree;
+        }
+    }
+
+    private static int FindMaxScenic(Grid<int> trees) => trees.Keys().Max(x => ScoreScenic(trees, x));
+
+    private static int ScoreScenic(Grid<int> trees, Position centre)
+    {
+        var up = GetSpanWithX(centre.X, 0, centre.Y).Reverse();
+        var down = GetSpanWithX(centre.X, centre.Y, trees.Height - 1);
+        var left = GetSpanWithY(centre.Y, 0, centre.X).Reverse();
+        var right = GetSpanWithY(centre.Y, centre.X, trees.Width - 1);
+
+        var sightLines = new[] { up, down, left, right };
+        return sightLines.Select(x => CountSightLineInSpan(trees, x)).Aggregate(1, (a, b) => a * b);
+    }
+
+    private static int CountSightLineInSpan(Grid<int> trees, IEnumerable<Position> span)
+    {
+        int? height = null;
+        var count = 0;
+
+        foreach (var position in span)
+        {
+            if (height == null)
+            {
+                height = trees[position];
+                continue;
+            }
+            
+            count++;
+
+            if (height <= trees[position])
+            {
+                break;
+            }
         }
 
-        return visible;
+        return count;
     }
+    
+    private static IEnumerable<Position> GetSpanWithX(int x, int startY, int endY) => Enumerable.Range(startY, endY - startY + 1).Select(y => new Position(x, y));
+    private static IEnumerable<Position> GetSpanWithY(int y, int startX, int endX) => Enumerable.Range(startX, endX - startX + 1).Select(x => new Position(x, y));
 
     public record Model(Grid<int> Trees);
 }
