7e85e95fd1e68fe38b88ad85cd322040e54cf7c2
Will Smith <will@toxon.co.uk>
2022-12-11 07:40:25 +0000

    Solve Day11

---
+++ AdventOfCode2022/Day11.Parser.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Immutable;
+using Superpower;
+using Superpower.Parsers;
+using Superpower.Tokenizers;
+
+namespace AdventOfCode2022;
+
+public partial class Day11
+{
+    protected override Tokenizer<TokenType> Tokenizer => new TokenizerBuilder<TokenType>()
+        .Ignore(Span.WhiteSpace)
+        .Ignore(Character.EqualTo(':'))
+        .Match(Span.EqualTo("Monkey"), TokenType.Monkey)
+        .Match(Span.EqualTo("Starting items"), TokenType.StartingItems)
+        .Match(Span.EqualTo("Operation: new ="), TokenType.Operation)
+        .Match(Span.EqualTo("old"), TokenType.Old)
+        .Match(Span.EqualTo("Test: divisible by"), TokenType.Test)
+        .Match(Span.EqualTo("If true: throw to monkey"), TokenType.True)
+        .Match(Span.EqualTo("If false: throw to monkey"), TokenType.False)
+        .Match(Character.EqualTo('+'), TokenType.Plus)
+        .Match(Character.EqualTo('*'), TokenType.Star)
+        .Match(Character.EqualTo(','), TokenType.Comma)
+        .Match(Numerics.Integer, TokenType.Number)
+        .Build();
+
+    private static readonly TokenListParser<TokenType, int> NumberParser = Token.EqualTo(TokenType.Number).Select(x => int.Parse(x.ToStringValue()));
+    private static readonly TokenListParser<TokenType, int[]> NumberListParser = NumberParser.ManyDelimitedBy(Token.EqualTo(TokenType.Comma));
+
+    private static readonly TokenListParser<TokenType, MonkeyExpression> ExpressionNumberParser = NumberParser.Select(x => (MonkeyExpression) new MonkeyExpression.Constant(x));
+    private static readonly TokenListParser<TokenType, MonkeyExpression> ExpressionOldParser = Token.EqualTo(TokenType.Old).Select(_ => (MonkeyExpression) new MonkeyExpression.Old());
+    private static readonly TokenListParser<TokenType, MonkeyExpression> ExpressionAtomParser = ExpressionNumberParser.Or(ExpressionOldParser);
+    private static readonly TokenListParser<TokenType, MonkeyExpression> ExpressionParser = ExpressionAtomParser.Then(Token.EqualTo(TokenType.Plus).Or(Token.EqualTo(TokenType.Star))).Then(ExpressionAtomParser)
+        .Select(x => x.Item1.Item2.Kind == TokenType.Plus ? (MonkeyExpression)new MonkeyExpression.Add(x.Item1.Item1, x.Item2) : new MonkeyExpression.Mult(x.Item1.Item1, x.Item2));
+    
+    private static readonly TokenListParser<TokenType, MonkeyAction> ActionParser = Token.Sequence(TokenType.Test, TokenType.Number, TokenType.True, TokenType.Number, TokenType.False, TokenType.Number)
+        .Select(x => new MonkeyAction(int.Parse(x[1].ToStringValue()), int.Parse(x[3].ToStringValue()), int.Parse(x[5].ToStringValue())));
+    
+    private static readonly TokenListParser<TokenType, Monkey> MonkeyParser =
+        from id in Token.EqualTo(TokenType.Monkey).IgnoreThen(NumberParser)
+        from items in Token.EqualTo(TokenType.StartingItems).IgnoreThen(NumberListParser)
+        from expr in Token.EqualTo(TokenType.Operation).IgnoreThen(ExpressionParser)
+        from action in ActionParser
+        select new Monkey(id, ToQueue(items.Select(x => (long)x)), expr, action);
+    
+    private static readonly TokenListParser<TokenType, Model> ModelParser = MonkeyParser.Many().AtEnd().Select(x => new Model(x.ToImmutableList()));
+    
+    private static ImmutableQueue<T> ToQueue<T>(IEnumerable<T> items) => 
+        items.Aggregate(ImmutableQueue<T>.Empty, (current, item) => current.Enqueue(item));
+
+    protected override TokenListParser<TokenType, Model> Parser => ModelParser;
+
+    public enum TokenType
+    {
+        Number,
+        Plus,
+        Star,
+        Comma,
+        
+        Monkey,
+        StartingItems,
+        Operation,
+        Old,
+        Test,
+        True,
+        False,
+    }
+}
--- AdventOfCode2022/Day11.cs
+++ AdventOfCode2022/Day11.cs
@@ -1,12 +1,100 @@
+using System.Collections.Immutable;
+using Superpower;
+
 namespace AdventOfCode2022;
 
 [Day]
-public partial class Day11 : Day<Day11.Model, int, int>
-{
-    protected override Model Parse(string input) => throw new NotImplementedException();
+public partial class Day11 : ParseDay<Day11.Model, Day11.TokenType, long, long>
+{ 
+    private const string Sample = "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1";
+  
+    [Sample(Sample, 10605L)]
+    protected override long Part1(Model model)
+    {
+        for (var round = 0; round < 20; round++)
+        {
+            model = RunRound(model, true);
+        }
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+        var top2 = model.Monkeys.Select(x => x.ItemsInspected).OrderByDescending(x => x).Take(2).ToList();
+        return top2[0] * top2[1];
+    }
 
-    public record Model;
+    [Sample(Sample, 2713310158L)]
+    protected override long Part2(Model model)
+    {
+        for (var round = 0; round < 10_000; round++)
+        {
+            model = RunRound(model, false);
+        }
+
+        var top2 = model.Monkeys.Select(x => x.ItemsInspected).OrderByDescending(x => x).Take(2).ToList();
+        return top2[0] * top2[1];
+    }
+
+    private Model RunRound(Model model, bool relaxed)
+    {
+        for (var i = 0; i < model.Monkeys.Count; i++)
+        {
+            model = RunTurn(model, i, relaxed);
+        }
+
+        return model;
+    }
+
+    private Model RunTurn(Model model, int id, bool relaxed)
+    {
+        var mod = model.Monkeys.Aggregate(1L, (acc, monkey) => acc * monkey.Action.Modulo); 
+        
+        while (!model.Monkeys[id].Items.IsEmpty)
+        {
+            var monkey = model.Monkeys[id];
+
+            model = model.Dequeue(id, out var item);
+            var newItem = Evaluate(monkey.WorryExpression, item);
+            if (relaxed)
+            {
+                newItem /= 3;
+            }
+            newItem %= mod;
+
+            var test = newItem % monkey.Action.Modulo == 0;
+            model = model.Enqueue(test ? monkey.Action.DivisibleTarget : monkey.Action.IndivisibleTarget, newItem);
+        }
+        
+        return model;
+    }
+
+    private long Evaluate(MonkeyExpression expr, long old) =>
+        expr switch
+        {
+            MonkeyExpression.Constant constant => constant.Value,
+            MonkeyExpression.Old => old,
+
+            MonkeyExpression.Add add => Evaluate(add.Left, old) + Evaluate(add.Right, old),
+            MonkeyExpression.Mult mult => Evaluate(mult.Left, old) * Evaluate(mult.Right, old),
+
+            _ => throw new ArgumentOutOfRangeException(nameof(expr))
+        };
+
+    public record Model(ImmutableList<Monkey> Monkeys)
+    {
+        public Model Enqueue(int id, long item) => new(Monkeys.SetItem(id, Monkeys[id].Enqueue(item)));
+        public Model Dequeue(int id, out long item) => new(Monkeys.SetItem(id, Monkeys[id].Dequeue(out item)));
+    }
+
+    public record Monkey(int Id, ImmutableQueue<long> Items, MonkeyExpression WorryExpression, MonkeyAction Action, long ItemsInspected = 0)
+    {
+        public Monkey Enqueue(long item) => this with { Items = Items.Enqueue(item) };
+        public Monkey Dequeue(out long item) => this with { Items = Items.Dequeue(out item), ItemsInspected = ItemsInspected + 1 };
+    }
+    public abstract record MonkeyExpression
+    {
+        public record Old : MonkeyExpression;
+        public record Constant(long Value) : MonkeyExpression;
+        public record Add(MonkeyExpression Left, MonkeyExpression Right) : MonkeyExpression;
+        public record Mult(MonkeyExpression Left, MonkeyExpression Right) : MonkeyExpression;
+    }
+    public record MonkeyAction(long Modulo, int DivisibleTarget, int IndivisibleTarget);
+
 }
