d9188e67281e48cbc2f4ea5a4af6d020432657d5
Will Smith <will@toxon.co.uk>
2022-12-12 13:14:32 +0000

    Solve Day12

--- AdventOfCode.Core/Grid/GridParser.cs
+++ AdventOfCode.Core/Grid/GridParser.cs
@@ -20,22 +20,23 @@ public static class GridParser
         return new Grid<bool>(grid, width);
     } 
     
-    public static Grid<int> ParseInt(string input)
+    public static Grid<int> ParseInt(string input) => ParseChar(input, x => x - '0');
+
+    public static Grid<T> ParseChar<T>(string input, Func<char, T> selector)
     {
         var lines = input.Split('\n');
         var width = lines[0].Length;
 
-        var grid = new int[width * lines.Length];
+        var grid = new T[width * lines.Length];
         for (var y = 0; y < lines.Length; y++)
         {
             var line = lines[y];
             for (var x = 0; x < line.Length; x++)
             {
-                grid[y * width + x] = line[x] - '0';
+                grid[y * width + x] = selector(line[x]);
             }
         }
         
-        return new Grid<int>(grid, width);
-    } 
-
+        return new Grid<T>(grid, width);
+    }
 }
--- AdventOfCode2022/Day12.cs
+++ AdventOfCode2022/Day12.cs
@@ -3,10 +3,97 @@ namespace AdventOfCode2022;
 [Day]
 public partial class Day12 : Day<Day12.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    private const string Sample = "Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi";
+    
+    protected override Model Parse(string input) => Model.Build(GridParser.ParseChar(input, x => x - 'a'));
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample(Sample, 31)]
+    protected override int Part1(Model input)
+    {
+        // A*
+        var start = input.Start;
+        var end = input.End;
 
-    public record Model;
+        var openSet = new PriorityQueue<Position, int>();
+        openSet.Enqueue(start, H(start));
+        
+        var cameFrom = new Dictionary<Position, Position>();
+        var gScore = new Dictionary<Position, int>
+        {
+            [start] = 0
+        };
+
+        while (openSet.Count > 0)
+        {
+            var current = openSet.Dequeue();
+            if (current == end)
+            {
+                return CountPath(cameFrom, end, start);
+            }
+            
+            foreach (var neighbour in current.OrthogonalNeighbours())
+            {
+                if (!input.Map.IsValid(neighbour))
+                {
+                    continue;
+                }
+                if (input.Map[neighbour] - input.Map[current] > 1)
+                {
+                    continue;
+                }
+
+                var newGScore = gScore[current] + 1;
+                var previousGScore = gScore.TryGetValue(neighbour, out var x) ? x : int.MaxValue;
+
+                if (newGScore < previousGScore)
+                {
+                    cameFrom[neighbour] = current;
+                    gScore[neighbour] = newGScore;
+                    openSet.Enqueue(neighbour, newGScore + H(neighbour));
+                }
+            }
+        }
+        
+        throw new Exception("no solution");
+        
+        int H(Position p) => (end - p).TaxiDistance();  
+    }
+    
+    [Sample(Sample, 29)]
+    protected override int Part2(Model input)
+    {
+        return OptimisedSearch.Solve((Current: input.End, Distance: 0), 0, IsGoal, Next, x => false, x => x.Distance).Distance;
+        
+        bool IsGoal((Position Current, int Distance) x) => input.Map[x.Current] == 0;
+        IEnumerable<(Position Current, int Distance)> Next((Position Current, int Distance) x) => 
+            x.Current.OrthogonalNeighbours().Where(n => input.Map.IsValid(n) && input.Map[x.Current] - input.Map[n] <= 1).Select(n => (n, x.Distance + 1));
+    }
+
+    private static int CountPath(IReadOnlyDictionary<Position, Position> cameFrom, Position end, Position start)
+    {
+        var position = end;
+        var count = 0;
+        
+        while (position != start)
+        {
+            count++;
+            position = cameFrom[position];
+        }
+
+        return count;
+    }
+    
+    public record Model(Grid<int> Map, Position Start, Position End)
+    {
+        public static Model Build(Grid<int> map)
+        {
+            var start = map.Keys().First(x => map[x] == 'S' - 'a');
+            var end = map.Keys().First(x => map[x] == 'E' - 'a');
+
+            map[start] = 'a' - 'a';
+            map[end] = 'z' - 'a';
+            
+            return new Model(map, start, end);
+        }
+    }
 }
