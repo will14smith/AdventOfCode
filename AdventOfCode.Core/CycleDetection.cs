ecbd253c0dc73ca075c3514726cdb69eea34dcc8
Will Smith <will@toxon.co.uk>
2022-12-17 22:35:26 +0000

    Solve Day17.2

---
+++ AdventOfCode.Core/CycleDetection.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Core;
+
+public static class CycleDetection
+{
+    public static (int Offset, int Length) Detect<T>(Func<T> initialStateFn, Func<T, T> applyFn, Func<T, T, bool> equalFn)
+    {
+        // https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_tortoise_and_hare
+        
+        var tortoise = initialStateFn();
+        var hare = initialStateFn();
+
+        do
+        {
+            tortoise = applyFn(tortoise);
+            hare = applyFn(applyFn(hare));
+        } while (!equalFn(tortoise, hare));
+
+        var offset = 0;
+        tortoise = initialStateFn();
+        while (!equalFn(tortoise, hare))
+        {
+            tortoise = applyFn(tortoise);
+            hare = applyFn(hare);
+            offset++;
+        }
+
+        var length = 1;
+        hare = applyFn(tortoise);
+        while (!equalFn(tortoise, hare))
+        {
+            hare = applyFn(hare);
+            length++;
+        }
+
+        return (offset, length);
+    }
+}
--- AdventOfCode2022/Day17.cs
+++ AdventOfCode2022/Day17.cs
@@ -1,9 +1,7 @@
-using System.Text;
-
 namespace AdventOfCode2022;
 
 [Day]
-public partial class Day17 : Day<Day17.Model, int, int>
+public partial class Day17 : Day<Day17.Model, int, long>
 {
     private const string Sample = @">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
 
@@ -22,74 +20,77 @@ public partial class Day17 : Day<Day17.Model, int, int>
 
         for (var rockIndex = 0; rockIndex < rockCount; rockIndex++)
         {
-            // drop rock
             var rockShape = RockShapes[rockIndex % RockShapes.Count];
-            var rock = MaterialiseRock(map, rockShape);
-
-            // Print(map, rock);
-
-            while (true)
-            {
-                var direction = input.Wind[windIndex++ % input.Wind.Count];
-                var pushedRock = ApplyForce(rock, new Position(direction == Direction.Left ? -1 : 1, 0));
-                if (!map.Intersects(pushedRock))
-                {
-                    rock = pushedRock;
-                }
-                
-                var droppedRock = ApplyForce(rock, new Position(0, -1));
-
-                if (!map.Intersects(droppedRock))
-                {
-                    rock = droppedRock;
-                    continue;
-                }
-                
-                map.Add(rock);
-                break;
-            }
-        }
-
-        return map.HighestRock;
-    }
-
-    private void Print(Map map, Position[] rock)
-    {
-        for (var y = rock.Max(x => x.Y); y >= 0; y--)
-        {
-            var line = new StringBuilder(9);
-
-            line.Append('|');
             
-            for (var x = 0; x < 7; x++)
-            {
-                if (map.Rocks.Contains(new Position(x, y)))
-                {
-                    line.Append('#');
-                } 
-                else if (rock.Contains(new Position(x, y)))
-                {
-                    line.Append('@');
-                }
-                else
-                {
-                    line.Append('.');
-                }
-            }
-
-            line.Append('|');
-            
-            Output.WriteLine(line.ToString());
+            windIndex = DropRock(input, windIndex, rockShape, map);
         }
         
-        Output.WriteLine("+-------+");
-        Output.WriteLine("\n\n");
+        return map.HighestRock;
     }
-
-    [Sample(Sample, -1)]
-    protected override int Part2(Model input) => throw new NotImplementedException();
     
-    private Position[] MaterialiseRock(Map map, RockShape rockShape)
+    [Sample(Sample, 1514285714288L)]
+    protected override long Part2(Model input)
+    {
+        var (cycleOffset, cycleLength) = CycleDetection.Detect(() => State.New, Apply, EqualEnough);
+
+        var stateAfterOffset = State.New;
+        for (var i = 0; i < cycleOffset; i++)
+        {
+            stateAfterOffset = Apply(stateAfterOffset);
+        }
+        
+        var stateAfterFirstCycle = State.New;
+        for (var i = 0; i < cycleOffset+cycleLength; i++)
+        {
+            stateAfterFirstCycle = Apply(stateAfterFirstCycle);
+        }
+
+        var height = 0L;
+        var remaining = 1000000000000L;
+
+        height += stateAfterOffset.Map.HighestRock;
+        remaining -= cycleOffset;
+
+        var bulkDrop = remaining / cycleLength;
+        height += bulkDrop * (stateAfterFirstCycle.Map.HighestRock - stateAfterOffset.Map.HighestRock);
+        remaining %= cycleLength;
+        
+        var stateAfterFirstCycleAndRemainder = State.New;
+        for (var i = 0; i < cycleOffset+cycleLength+remaining; i++)
+        {
+            stateAfterFirstCycleAndRemainder = Apply(stateAfterFirstCycleAndRemainder);
+        }
+
+        height += stateAfterFirstCycleAndRemainder.Map.HighestRock - stateAfterFirstCycle.Map.HighestRock;
+
+        return height;
+
+        State Apply(State state)
+        {
+            var (map, wind, rock) = state;
+            
+            var rockShape = RockShapes[rock];
+            rock = (rock + 1) % RockShapes.Count;
+
+            wind = DropRock(input, wind, rockShape, map);
+
+            return new State(map, wind, rock);
+        }
+        
+        bool EqualEnough(State a, State b) => GetStateSummary(a) == GetStateSummary(b);
+
+        (int WindIndex, int RockIndex, string TopNRows) GetStateSummary(State state)
+        {
+            const int N = 64;
+            var (map, wind, rock) = state;
+            
+            var topNRows = string.Join(";", map.Rocks.Where(x => x.Y >= map.HighestRock - N).Select(p => $"{p.X},{(map.HighestRock - p.Y) % (N +1)}"));
+
+            return (wind, rock, topNRows);
+        }
+    }
+    
+    private static Position[] MaterialiseRock(Map map, RockShape rockShape)
     {
         var baseY = map.HighestRock + 1 + 3;
 
@@ -112,7 +113,37 @@ public partial class Day17 : Day<Day17.Model, int, int>
         return rock.ToArray();
     }
     
-    private Position[] ApplyForce(Position[] rock, Position force)
+    private static int DropRock(Model input, int windIndex, RockShape rockShape, Map map)
+    {
+        var rock = MaterialiseRock(map, rockShape);
+
+        while (true)
+        {
+            var direction = input.Wind[windIndex];
+            windIndex = (windIndex + 1) % input.Wind.Count;
+            
+            var pushedRock = ApplyForce(rock, new Position(direction == Direction.Left ? -1 : 1, 0));
+            if (!map.Intersects(pushedRock))
+            {
+                rock = pushedRock;
+            }
+
+            var droppedRock = ApplyForce(rock, new Position(0, -1));
+
+            if (!map.Intersects(droppedRock))
+            {
+                rock = droppedRock;
+                continue;
+            }
+
+            map.Add(rock);
+            break;
+        }
+
+        return windIndex;
+    }
+    
+    private static Position[] ApplyForce(Position[] rock, Position force)
     {
         var newRock = new Position[rock.Length];
 
@@ -133,21 +164,27 @@ public partial class Day17 : Day<Day17.Model, int, int>
 
     public record RockShape(bool[][] Cells);
 
-    public record Map(HashSet<Position> Rocks)
+    public record Map(HashSet<Position> RockSet, List<Position> Rocks)
     {
-        public static Map Initial => new(new HashSet<Position>());
+        public static Map Initial => new(new HashSet<Position>(), new List<Position>());
 
         public int HighestRock { get; private set; }
 
         public bool Intersects(Position[] rock)
         {
-            return rock.Any(p => p.Y <= 0 || p.X is < 0 or >= 7 || Rocks.Contains(p));
+            return rock.Any(p => p.Y <= 0 || p.X is < 0 or >= 7 || RockSet.Contains(p));
         }
 
         public void Add(Position[] rock)
         {
-            Rocks.UnionWith(rock);
+            RockSet.UnionWith(rock);
+            Rocks.AddRange(rock);
             HighestRock = Math.Max(HighestRock, rock.Max(x => x.Y));
         }
+    }
+    
+    public record State(Map Map, int Wind, int Rock)
+    {
+        public static State New => new(Map.Initial, 0, 0);
     }
 }
