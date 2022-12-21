bdd617e77f596ce8c92565c47294194db83c2196
Will Smith <will@toxon.co.uk>
2022-12-21 19:59:13 +0000

    Refactor Day19
    Went from 25 minutes -> 4 minutes -> 0.6s
    sse2 for the win

---
+++ AdventOfCode2022/Day19.Models.cs
@@ -0,0 +1,77 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace AdventOfCode2022;
+
+public partial class Day19
+{
+    public record Blueprint(int Id, Resources[] Robots)
+    {
+        public Resources MaxRobots { get; private set; }
+
+        public static Blueprint Create(int id, Resources ore, Resources clay, Resources obsidian, Resources geode)
+        {
+            var blueprint = new Blueprint(id, new[] { ore, clay, obsidian, geode });
+
+            blueprint.MaxRobots = new Resources(Vector128.Create(
+                blueprint.Robots.Max(x => x.Get(Resource.Ore)),
+                blueprint.Robots.Max(x => x.Get(Resource.Clay)),
+                blueprint.Robots.Max(x => x.Get(Resource.Obsidian)),
+                100_000
+            ));
+
+            return blueprint;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Resources Get(Resource type) => Robots[(int)type];
+    }
+
+    public readonly struct Resources
+    {
+        public static readonly Resources Empty = new(Vector128<int>.Zero);
+        public static readonly Resources Ore = new(Vector128.Create(1, 0, 0, 0));
+        public static readonly Resources Clay = new(Vector128.Create(0, 1, 0, 0));
+        public static readonly Resources Obsidian = new(Vector128.Create(0, 0, 1, 0));
+        public static readonly Resources Geode = new(Vector128.Create(0, 0, 0, 1));
+
+        public readonly Vector128<int> Values;
+        public Resources(Vector128<int> values)
+        {
+            Values = values;
+        }
+
+        public static Resources operator +(Resources a, Resources b) =>
+            new(Sse2.Add(a.Values, b.Values));
+
+        public static Resources operator -(Resources a, Resources b) =>
+            new(Sse2.Subtract(a.Values, b.Values));
+
+        public static Resources operator *(int a, Resources b) => new(a * b.Values);
+
+        public static bool operator >(Resources a, Resources b) =>
+            Sse2.MoveMask(Sse2.CompareGreaterThan(a.Values, b.Values).AsByte()) != 0;
+
+        public static bool operator <(Resources a, Resources b) =>
+            Sse2.MoveMask(Sse2.CompareLessThan(a.Values, b.Values).AsByte()) != 0;
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Get(Resource resource) => Values[(int)resource];
+    }
+
+    public enum Resource
+    {
+        Ore,
+        Clay,
+        Obsidian,
+        Geode,
+    }
+
+    public record State(Blueprint Blueprint, Resources Current, Resources Robots)
+    {
+        public static State New(Blueprint blueprint) =>
+            new(blueprint, Resources.Empty, Resources.Ore);
+    }
+}
--- AdventOfCode2022/Day19.cs
+++ AdventOfCode2022/Day19.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using Superpower;
 using Superpower.Parsers;
 
@@ -10,180 +12,121 @@ public partial class Day19 : ParseLineDay<Day19.Blueprint, int, int>
 
     protected override TextParser<Blueprint> LineParser =>
         from id in Span.EqualTo("Blueprint ").IgnoreThen(Numerics.IntegerInt32)
-        from ore in Span.EqualTo(": Each ore robot costs ").IgnoreThen(Numerics.IntegerInt32).Select(x => new Resources(x, 0, 0, 0))
-        from clay in Span.EqualTo(" ore. Each clay robot costs ").IgnoreThen(Numerics.IntegerInt32).Select(x => new Resources(x, 0, 0, 0))
-        from obsidian in Span.EqualTo(" ore. Each obsidian robot costs ").IgnoreThen(Numerics.IntegerInt32).ThenIgnore(Span.EqualTo(" ore and ")).Then(Numerics.IntegerInt32).Select(x => new Resources(x.Item1, x.Item2, 0, 0))
-        from geode in Span.EqualTo(" clay. Each geode robot costs ").IgnoreThen(Numerics.IntegerInt32).ThenIgnore(Span.EqualTo(" ore and ")).Then(Numerics.IntegerInt32).ThenIgnore(Span.EqualTo(" obsidian.")).Select(x => new Resources(x.Item1, 0, x.Item2, 0))
+        from ore in Span.EqualTo(": Each ore robot costs ").IgnoreThen(Numerics.IntegerInt32).Select(x => x * Resources.Ore)
+        from clay in Span.EqualTo(" ore. Each clay robot costs ").IgnoreThen(Numerics.IntegerInt32).Select(x => x * Resources.Ore)
+        from obsidian in Span.EqualTo(" ore. Each obsidian robot costs ").IgnoreThen(Numerics.IntegerInt32).ThenIgnore(Span.EqualTo(" ore and ")).Then(Numerics.IntegerInt32).Select(x => x.Item1 * Resources.Ore + x.Item2 * Resources.Clay)
+        from geode in Span.EqualTo(" clay. Each geode robot costs ").IgnoreThen(Numerics.IntegerInt32).ThenIgnore(Span.EqualTo(" ore and ")).Then(Numerics.IntegerInt32).ThenIgnore(Span.EqualTo(" obsidian.")).Select(x => x.Item1 * Resources.Ore + x.Item2 * Resources.Obsidian)
         select Blueprint.Create(id, ore, clay, obsidian, geode);
 
     [Sample(Sample, 33)]
     protected override int Part1(IEnumerable<Blueprint> input)
     {
-        return input.AsParallel().Select(blueprint => FindMostGeodes(State.New(blueprint))).Sum(x => x.Blueprint.Id * x.Current.Geode);
+        return input.AsParallel().Select(blueprint => blueprint.Id * FindMostGeodes(State.New(blueprint))).Sum();
     }
     
     [Sample(Sample, 3472)]
     protected override int Part2(IEnumerable<Blueprint> input)
     {
-        return input.Take(3).AsParallel().Select(blueprint => FindMostGeodes(State.New(blueprint), 32)).Aggregate(1, (acc, state) => acc * state.Current.Geode);
+        return input.Take(3).AsParallel().Select(blueprint => FindMostGeodes(State.New(blueprint), 32)).Aggregate(1, (acc, geodes) => acc * geodes);
     }
 
-    private static State FindMostGeodes(State state, int time = 24)
+    private static int FindMostGeodes(State state, int time = 24)
     {
-        var best = state;
-
-        var couldBuyAnyPrevious = false;
+        var best = state.Current.Get(Resource.Geode);
         
-        foreach (var robotType in Resources.List)
+        var shouldBuildRobot = Sse2.CompareLessThan(state.Robots.Values, state.Blueprint.MaxRobots.Values);
+
+        if(state.Robots.Values[(int)Resource.Obsidian] > 0)
         {
-            var timeUntilNextRobot = TimeUntilRobotCanBeBuilt(state, robotType);
-            var shouldBuildRobot = ShouldBuildRobot(state, robotType);
-
-            if (!couldBuyAnyPrevious && robotType == Resource.Geode)
+            if (shouldBuildRobot[(int)Resource.Geode] != 0)
             {
-                while (time > 0)
-                {
-                    timeUntilNextRobot = TimeUntilRobotCanBeBuilt(state, robotType)!;
+                var timeUntilNextRobot = TimeUntilRobotCanBeBuilt(state, Resource.Geode);
 
-                    if (timeUntilNextRobot.Value > time)
-                    {
-                        break;
-                    }
-                    
-                    state = BuyRobot(SimulateTime(state, timeUntilNextRobot.Value + 1), robotType);
-                    time -= timeUntilNextRobot.Value + 1;
+                if (timeUntilNextRobot < time)
+                {
+                    var candidate = FindMostGeodes(SimulateTimeThenBuyRobot(state, timeUntilNextRobot.Value, Resource.Geode), time - timeUntilNextRobot.Value);
+                    best = Math.Max(best, candidate);
                 }
 
-                return SimulateTime(state, time);
-            }
-            
-            if (timeUntilNextRobot.HasValue && shouldBuildRobot)
-            {
-                couldBuyAnyPrevious = true;
+                best = Math.Max(best, state.Current.Get(Resource.Geode) + state.Robots.Get(Resource.Geode) * time);
                 
-                if (timeUntilNextRobot.Value < time)
+                if (timeUntilNextRobot == 1)
                 {
-                    var candidate = FindMostGeodes(BuyRobot(SimulateTime(state, timeUntilNextRobot.Value + 1), robotType), time - timeUntilNextRobot.Value - 1);
-                    if (candidate.Current.Geode > best.Current.Geode)
-                    {
-                        best = candidate;
-                    }
+                    // at the point where 1 geode robot is produced per minute
+                    return best;
                 }
             }
         }
 
+        if (shouldBuildRobot[(int)Resource.Ore] != 0)
+        {
+            var timeUntilNextRobot = TimeUntilRobotCanBeBuilt(state, Resource.Ore);
+
+            if (timeUntilNextRobot < time)
+            {
+                var candidate = FindMostGeodes(SimulateTimeThenBuyRobot(state, timeUntilNextRobot.Value, Resource.Ore), time - timeUntilNextRobot.Value);
+                best = Math.Max(best, candidate);
+            }
+        }
+
+        if (shouldBuildRobot[(int)Resource.Clay] != 0)
+        {
+            var timeUntilNextRobot = TimeUntilRobotCanBeBuilt(state, Resource.Clay);
+
+            if (timeUntilNextRobot < time)
+            {
+                var candidate = FindMostGeodes(SimulateTimeThenBuyRobot(state, timeUntilNextRobot.Value, Resource.Clay), time - timeUntilNextRobot.Value);
+                best = Math.Max(best, candidate);
+            }
+        }
+
+        if(state.Robots.Values[(int)Resource.Clay] > 0)
+        {
+            if (shouldBuildRobot[(int)Resource.Obsidian] != 0)
+            {
+                var timeUntilNextRobot = TimeUntilRobotCanBeBuilt(state, Resource.Obsidian);
+
+                if (timeUntilNextRobot < time)
+                {
+                    var candidate = FindMostGeodes(SimulateTimeThenBuyRobot(state, timeUntilNextRobot.Value, Resource.Obsidian), time - timeUntilNextRobot.Value);
+                    best = Math.Max(best, candidate);
+                }
+            }
+        }
+        
         return best;
     }
     
     private static int? TimeUntilRobotCanBeBuilt(State state, Resource robotType)
     {
-        var cost = state.Blueprint.Get(robotType);
-
-        var time = 0;
+        var current = state.Current.Values;
+        var cost = state.Blueprint.Get(robotType).Values;
+        var production = state.Robots.Values;
         
-        foreach (var resource in Resources.List)
-        {
-            var resourceTime = TimeUntilResourceReady(state.Current, cost, state.Robots, resource);
-            if (resourceTime == null) return null;
-            
-            time = Math.Max(time, resourceTime.Value);
-        }
-        
-        return time;
-    }
+        var required = cost - current + production - Vector128.Create(1, 1, 1, 1);
+        var requiredMask = Sse2.CompareGreaterThan(required, Vector128<int>.Zero);
+        required = Sse2.And(required, requiredMask);
 
-    private static int? TimeUntilResourceReady(Resources current, Resources cost, Resources production, Resource resource)
-    {
-        var resourceCost = cost.Get(resource);
-        if (resourceCost == 0) return 0;
+        var productionMask = Sse2.CompareEqual(production, Vector128<int>.Zero);
+        var missingProduction = Sse2.And(requiredMask, productionMask);
+        if (Sse2.MoveMask(missingProduction.AsByte()) != 0) return null;
 
-        var resourceProduction = production.Get(resource);
-        if (resourceProduction == 0) return null;
-
-        var resourceCurrent = current.Get(resource);
-        var time = (resourceCost - resourceCurrent + resourceProduction - 1) / resourceProduction;
-        return time < 0 ? 0 : time;
-    }
-
-    private static bool ShouldBuildRobot(State state, Resource resource) => state.Robots.Get(resource) < state.Blueprint.MaxRobots.Get(resource);
-
-    private static State SimulateTime(State state, int time)
-    {
-        return state with { Current = state.Current + time * state.Robots };
+        var time = required / (production + productionMask);
+        // plus 1 since it takes a minute to build
+        return Math.Max(Math.Max(time[0], time[1]), Math.Max(time[2], time[3])) + 1;
     }
     
-    private static State BuyRobot(State state, Resource robotType)
+    private static State SimulateTimeThenBuyRobot(State state, int time, Resource robotType)
     {
         var cost = state.Blueprint.Get(robotType);
         var robotDelta = robotType switch
         {
-            Resource.Ore => Resources.Empty with { Ore = 1 },
-            Resource.Clay => Resources.Empty with { Clay = 1 },
-            Resource.Obsidian => Resources.Empty with { Obsidian = 1 },
-            Resource.Geode => Resources.Empty with { Geode = 1 },
-        };
-        
-        return state with { Current = state.Current - cost, Robots = state.Robots + robotDelta };
-    }
-    
-    public record Blueprint(int Id, Resources Ore, Resources Clay, Resources Obsidian, Resources Geode)
-    {
-        public Resources MaxRobots { get; private set; }
-        
-        public static Blueprint Create(int id, Resources ore, Resources clay, Resources obsidian, Resources geode)
-        {
-            var blueprint = new Blueprint(id, ore, clay, obsidian, geode);
-
-            blueprint.MaxRobots = new Resources(
-                Math.Max(Math.Max(blueprint.Ore.Ore, blueprint.Clay.Ore), Math.Max(blueprint.Obsidian.Ore, blueprint.Geode.Ore)),
-                Math.Max(Math.Max(blueprint.Ore.Clay, blueprint.Clay.Clay), Math.Max(blueprint.Obsidian.Clay, blueprint.Geode.Clay)),
-                Math.Max(Math.Max(blueprint.Ore.Obsidian, blueprint.Clay.Obsidian), Math.Max(blueprint.Obsidian.Obsidian, blueprint.Geode.Obsidian)),
-                10_000
-            );
-
-            return blueprint;
-        }
-        
-        public Resources Get(Resource type) => type switch
-        {
-            Resource.Ore => Ore,
-            Resource.Clay => Clay,
-            Resource.Obsidian => Obsidian,
-            Resource.Geode => Geode,
+            Resource.Ore => Resources.Ore,
+            Resource.Clay => Resources.Clay,
+            Resource.Obsidian => Resources.Obsidian,
+            Resource.Geode => Resources.Geode,
         };
 
-    }
-
-    public record Resources(int Ore, int Clay, int Obsidian, int Geode)
-    {
-        public static readonly Resources Empty = new(0, 0, 0, 0);
-        public static readonly Resource[] List = { Resource.Ore, Resource.Clay, Resource.Obsidian, Resource.Geode };
-
-        public static Resources operator +(Resources a, Resources b) => new(a.Ore + b.Ore, a.Clay + b.Clay, a.Obsidian + b.Obsidian, a.Geode + b.Geode);
-        public static Resources operator -(Resources a, Resources b) => new(a.Ore - b.Ore, a.Clay - b.Clay, a.Obsidian - b.Obsidian, a.Geode - b.Geode);
-        public static Resources operator *(int a, Resources b) => new(a * b.Ore, a * b.Clay, a * b.Obsidian, a * b.Geode);
-
-        public static bool operator >(Resources a, Resources b) => a.Ore > b.Ore || a.Clay > b.Clay || a.Obsidian > b.Obsidian || a.Geode > b.Geode;
-        public static bool operator <(Resources a, Resources b) => a.Ore < b.Ore || a.Clay < b.Clay || a.Obsidian < b.Obsidian || a.Geode < b.Geode;
-
-        public int Get(Resource resource) => resource switch
-        {
-            Resource.Ore => Ore,
-            Resource.Clay => Clay,
-            Resource.Obsidian => Obsidian,
-            Resource.Geode => Geode,
-        };
-    }
-    public enum Resource
-    {
-        Ore,
-        Clay,
-        Obsidian,
-        Geode,
-    }
-
-    public record State(Blueprint Blueprint, Resources Current, Resources Robots)
-    {
-        public static State New(Blueprint blueprint) => new(blueprint, Resources.Empty, Resources.Empty with { Ore = 1 });
+        return state with { Current = state.Current + time * state.Robots - cost, Robots = state.Robots + robotDelta };
     }
 }
