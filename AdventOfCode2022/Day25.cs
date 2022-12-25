bff3fdbcde8444e1df090c1c708a4d92f1138b7c
Will Smith <will@fundapps.co>
2022-12-25 08:12:06 +0000

    Solve Day25

--- AdventOfCode2022/Day25.cs
+++ AdventOfCode2022/Day25.cs
@@ -1,12 +1,59 @@
+using System.Text;
+
 namespace AdventOfCode2022;
 
 [Day]
-public partial class Day25 : Day<Day25.Model, int, int>
+public partial class Day25 : LineDay<string, string, string>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    private const string Sample = "1=-0-2\n12111\n2=0=\n21\n2=01\n111\n20012\n112\n1=-1=\n1-12\n12\n1=\n122";
+    
+    protected override string ParseLine(string input) => input;
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample(Sample, "2=-1=0")]
+    protected override string Part1(IEnumerable<string> input) => Encode(input.Sum(Decode));
+    
+    protected override string Part2(IEnumerable<string> input) => "Merry Christmas.";
+    
+    private static long Decode(string str)
+    {
+        var value = 0L;
 
-    public record Model;
+        foreach (var c in str)
+        {
+            var digit = c switch
+            {
+                '2' => 2,
+                '1' => 1,
+                '0' => 0,
+                '-' => -1,
+                '=' => -2,
+            };
+            
+            value = value * 5 + digit;
+        }
+
+        return value;
+    }
+
+    private static string Encode(long value)
+    {
+        var sb = new StringBuilder();
+
+        while (value > 0)
+        {
+            var digit = (value + 2) % 5;
+            sb.Insert(0, digit switch
+            {
+                0 => '=',
+                1 => '-',
+                2 => '0',
+                3 => '1',
+                4 => '2',
+            });
+            
+            value = (value+2)/ 5;
+        }
+        
+        return sb.ToString();
+    }
 }
