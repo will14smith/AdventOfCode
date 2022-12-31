aa7f31254d03465dfac0e88b214bd46ae4fa2309
Will Smith <will@toxon.co.uk>
2022-12-31 12:12:20 +0000

    2016 Day18

--- AdventOfCode2016/Day18.cs
+++ AdventOfCode2016/Day18.cs
@@ -1,12 +1,41 @@
+using System.Numerics;
+using System.Text;
+
 namespace AdventOfCode2016;
 
 [Day]
 public partial class Day18 : Day<Day18.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    private const string Sample = @".^^.^.^^^^";
+    
+    protected override Model Parse(string input) => new(input);
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample(Sample, 38)]
+    protected override int Part1(Model input) => Solve(input, input.Row == Sample ? 10 : 40);
+    protected override int Part2(Model input) => Solve(input, 400_000);
 
-    public record Model;
+    private static int Solve(Model input, int rows)
+    {
+        var width = input.Row.Length;
+
+        var previous = UInt128.Zero;
+        for (var x = 0; x < width; x++)
+        {
+            previous |= input.Row[x] == '^' ? UInt128.One << x : UInt128.Zero;
+        }
+
+        var count = UInt128.PopCount(previous);
+
+        var mask = (UInt128.One << (width)) - UInt128.One;
+        for (var y = 1; y < rows; y++)
+        {
+            var next = ((previous >> 1) ^ (previous << 1)) & mask;
+            count += UInt128.PopCount(next);
+            previous = next;
+        }
+
+        return (width * rows) - (int)count;
+    }
+
+    public record Model(string Row);
 }
