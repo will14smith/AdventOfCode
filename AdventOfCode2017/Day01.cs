772f980f6147aad911f32b675036cd021ec6155f
Will Smith <will@toxon.co.uk>
2023-11-25 10:49:48 +0000

    2017 Day01

--- AdventOfCode2017/Day01.cs
+++ AdventOfCode2017/Day01.cs
@@ -3,10 +3,20 @@ namespace AdventOfCode2017;
 [Day]
 public partial class Day01 : Day<Day01.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    protected override Model Parse(string input) => new(input.Select(x => x - '0').ToList());
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample("1122", 3)]
+    [Sample("1111", 4)]
+    [Sample("1234", 0)]
+    [Sample("91212129", 9)]
+    protected override int Part1(Model input) => input.Digits.Select((x, i) => x == input.Digits[(i+1) % input.Digits.Count] ? x : 0).Sum();
 
-    public record Model;
+    [Sample("1212", 6)]
+    [Sample("1221", 0)]
+    [Sample("123425", 4)]
+    [Sample("123123", 12)]
+    [Sample("12131415", 4)]
+    protected override int Part2(Model input) => input.Digits.Select((x, i) => x == input.Digits[(i + input.Digits.Count/2) % input.Digits.Count] ? x : 0).Sum();
+
+    public record Model(IReadOnlyList<int> Digits);
 }
