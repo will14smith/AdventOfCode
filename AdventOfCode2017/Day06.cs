73b55ee119d073a0b87412d02768773742176fa4
Will Smith <will@toxon.co.uk>
2023-11-26 09:54:07 +0000

    2017 Day06

--- AdventOfCode2017/Day06.cs
+++ AdventOfCode2017/Day06.cs
@@ -3,10 +3,44 @@ namespace AdventOfCode2017;
 [Day]
 public partial class Day06 : Day<Day06.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    protected override Model Parse(string input) => new(input.Split('\t').Select(int.Parse).ToList());
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample("0\t2\t7\t0", 5)]
+    protected override int Part1(Model input)
+    {
+        var (offset, length) = CycleDetection.Detect(() => input.Blocks.ToArray(), Redistribute, StateEquals);
+        return offset + length;
+    }
+    
+    [Sample("2\t4\t1\t2", 4)]
+    protected override int Part2(Model input) => CycleDetection.Detect(() => input.Blocks.ToArray(), Redistribute, StateEquals).Length;
 
-    public record Model;
+    private static int[] Redistribute(int[] blocks)
+    {
+        var newBlocks = blocks.ToArray();
+        
+        // find highest
+        var max = newBlocks.Max();
+        var maxIndex = Array.IndexOf(newBlocks, max);
+            
+        // re-distribute
+        var (all, rem) = Math.DivRem(max, newBlocks.Length);
+            
+        newBlocks[maxIndex] = 0;
+        for (var i = 0; i < newBlocks.Length; i++)
+        {
+            newBlocks[i] += all;
+        }
+            
+        for (var i = 0; i < rem; i++)
+        {
+            newBlocks[(maxIndex+1+i) % newBlocks.Length]++;
+        }
+
+        return newBlocks;
+    }
+    
+    private static bool StateEquals(int[] a, int[] b) => string.Join(",", a) == string.Join(",", b);
+
+    public record Model(IReadOnlyList<int> Blocks);
 }
