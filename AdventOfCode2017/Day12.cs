e9f4197a7046b32f3bd95f95ced33f00e44478de
Will Smith <will@toxon.co.uk>
2023-11-26 16:51:46 +0000

    2017 Day12

--- AdventOfCode2017/Day12.cs
+++ AdventOfCode2017/Day12.cs
@@ -1,12 +1,70 @@
+using Superpower;
+using Superpower.Parsers;
+
 namespace AdventOfCode2017;
 
 [Day]
-public partial class Day12 : Day<Day12.Model, int, int>
+public partial class Day12 : ParseLineDay<Day12.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    public record Model(int Id, IReadOnlyList<int> Connections);
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample("0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5", 6)]
+    protected override int Part1(IEnumerable<Model> input)
+    {
+        var pipes = input.ToDictionary(x => x.Id);
+        var seen = ExploreConnections(pipes, 0);
 
-    public record Model;
+        return seen.Count;
+    }
+    
+    [Sample("0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5", 2)]
+    protected override int Part2(IEnumerable<Model> input)
+    {
+        var pipes = input.ToDictionary(x => x.Id);
+
+        var groups = 0;
+        while (pipes.Count > 0)
+        {
+            groups++;
+
+            var seen = ExploreConnections(pipes, pipes.First().Key);
+            
+            foreach (var id in seen)
+            {
+                pipes.Remove(id);
+            }
+        }
+
+        return groups;
+    }
+
+    private static IReadOnlySet<int> ExploreConnections(IReadOnlyDictionary<int, Model> pipes, int seed)
+    {
+        var seen = new HashSet<int>();
+        var queue = new Queue<int>();
+
+        queue.Enqueue(seed);
+
+        while (queue.Count > 0)
+        {
+            var id = queue.Dequeue();
+            var pipe = pipes[id];
+
+            foreach (var connection in pipe.Connections)
+            {
+                if (seen.Add(connection))
+                {
+                    queue.Enqueue(connection);
+                }
+            }
+        }
+
+        return seen;
+    }
+    
+    protected override TextParser<Model> LineParser { get; } =
+        from id in Numerics.IntegerInt32
+        from _ in Span.EqualTo(" <-> ")
+        from connections in Numerics.IntegerInt32.ManyDelimitedBy(Span.EqualTo(", "))
+        select new Model(id, connections);
 }
