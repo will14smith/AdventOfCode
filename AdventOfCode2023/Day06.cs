aaf8ff546368644bbc4f963bf193ccd9bff74b15
Will Smith <will@toxon.co.uk>
2023-11-30 23:09:02 +0000

    Setup 2023

---
+++ AdventOfCode2023/Day06.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2023;
+
+[Day]
+public partial class Day06 : Day<Day06.Model, int, int>
+{
+    protected override Model Parse(string input) => throw new NotImplementedException();
+
+    protected override int Part1(Model input) => throw new NotImplementedException();
+    protected override int Part2(Model input) => throw new NotImplementedException();
+
+    public record Model;
+}
