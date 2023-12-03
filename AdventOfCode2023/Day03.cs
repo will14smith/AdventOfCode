fa3518cbc5b35e5f0185a236e5e77b398971dd7f
Will Smith <will@toxon.co.uk>
2023-12-03 06:32:37 +0000

    Refactor 2023 Day03

--- AdventOfCode2023/Day03.cs
+++ AdventOfCode2023/Day03.cs
@@ -1,139 +1,75 @@
-﻿using System.Text;
-
-namespace AdventOfCode2023;
+﻿namespace AdventOfCode2023;
 
 [Day]
 public partial class Day03 : Day<Day03.Model, int, int>
 {
-    protected override Model Parse(string input) => new(GridParser.ParseChar(input, x => x));
-
-    [Sample(
-        "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..", 4361)]
-    protected override int Part1(Model input)
+    protected override Model Parse(string input)
     {
-        var symbols = new List<Position>();
-        foreach (var position in input.Grid.Keys())
+        var numbers = new Dictionary<Position, (Position Start, int Value)>();
+        var symbols = new Dictionary<Position, char>();
+        
+        var lines = input.Split('\n');
+        for (var y = 0; y < lines.Length; y++)
         {
-            var value = input.Grid[position];
-            if (value != '.' && !char.IsDigit(value))
+            ParseLine(y, lines[y]);
+        }
+
+        return new Model(numbers, symbols);
+
+        void ParseLine(int y, string line)
+        {
+            for (var x = 0; x < line.Length; x++)
             {
-                symbols.Add(position);
+                if (char.IsDigit(line[x]))
+                {
+                    ParseNumber(ref x, y, line);
+                }
+                else if (line[x] != '.')
+                {
+                    symbols[new Position(x, y)] = line[x];
+                }
             }
         }
 
-        var numbers = new Dictionary<Position, int>();
-        foreach (var symbol in symbols)
+        void ParseNumber(ref int x, int y, string line)
         {
-            foreach (var neighbour in symbol.StrictNeighbours())
-            {
-                if (!char.IsDigit(input.Grid[neighbour]))
-                {
-                    continue;
-                }
-                
-                var (start, value) = FindNumber(input.Grid, neighbour);
-                numbers[start] = value;
-            }
-        }
-
-        return numbers.Sum(x => x.Value);
-    }
-    
-    [Sample("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..", 467835)]
-    protected override int Part2(Model input)
-    {
-        var symbols = new HashSet<Position>();
-
-        foreach (var position in input.Grid.Keys())
-        {
-            var value = input.Grid[position];
-            if (value == '*')
-            {
-                symbols.Add(position);
-            }
-        }
-
-        var numbers = new Dictionary<Position, Dictionary<Position, int>>();
-        foreach (var symbol in symbols)
-        {
-            foreach (var neighbour in symbol.StrictNeighbours())
-            {
-                if (!char.IsDigit(input.Grid[neighbour]))
-                {
-                    continue;
-                }
-                
-                var (start, value) = FindNumber(input.Grid, neighbour);
-
-                if (!numbers.TryGetValue(symbol, out var symbolNumbers))
-                {
-                    symbolNumbers = new Dictionary<Position, int>();
-                    numbers[symbol] = symbolNumbers;
-                }
+            var startX = x;
+            var value = line[x] - '0';
                     
-                symbolNumbers[start] = value;
+            while (x + 1 < line.Length && char.IsDigit(line[x + 1]))
+            {
+                value = value * 10 + (line[++x] - '0');
+            }
+
+            var start = new Position(startX, y);
+
+            for (var nx = startX; nx <= x; nx++)
+            {
+                numbers.Add(new Position(nx, y), (start, value));
             }
         }
-
-        return numbers.Where(x => x.Value.Count == 2).Sum(x => x.Value.First().Value * x.Value.Last().Value);
-    }
-    
-    private static (Position Start, int Value) FindNumber(Grid<char> grid, Position position)
-    {
-        var start = FindNumberStart(grid, position);
-        var end = FindNumberEnd(grid, position);
-
-        var value = ParseNumber(grid, position, start, end);
-
-        return (position with { X = start }, value);
-    }
-    
-    private static int FindNumberStart(Grid<char> grid, Position position)
-    {
-        var start = position.X;
-        for (var x = position.X - 1; x >= 0; x--)
-        {
-            if (char.IsDigit(grid[x, position.Y]))
-            {
-                start = x;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return start;
-    }
-    
-    private static int FindNumberEnd(Grid<char> grid, Position position)
-    {
-        var end = position.X;
-        for (var x = position.X + 1; x < grid.Width; x++)
-        {
-            if (char.IsDigit(grid[x, position.Y]))
-            {
-                end = x;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return end;
-    }
-    
-    private static int ParseNumber(Grid<char> grid, Position position, int start, int end)
-    {
-        var str = new StringBuilder();
-        for (var x = start; x <= end; x++)
-        {
-            str.Append(grid[x, position.Y]);
-        }
-        return int.Parse(str.ToString());
     }
 
+    [Sample("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..", 4361)]
+    protected override int Part1(Model input) =>
+        input.Symbols.Keys
+            .SelectMany(symbol => GetNeighbouringNumbers(input.Numbers, symbol))
+            .Distinct()
+            .Sum(numberAtPosition => numberAtPosition.Value);
 
-    public record Model(Grid<char> Grid);
+    [Sample("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..", 467835)]
+    protected override int Part2(Model input) =>
+        input.Symbols
+            .Where(x => x.Value == '*')
+            .Select(gear => GetNeighbouringNumbers(input.Numbers, gear.Key).ToList())
+            .Where(neighbours => neighbours.Count == 2)
+            .Sum(neighbours => neighbours[0].Value * neighbours[1].Value);
+
+    private static IEnumerable<(Position Start, int Value)> GetNeighbouringNumbers(IReadOnlyDictionary<Position, (Position Start, int Value)> numbers, Position position) =>
+        position.StrictNeighbours()
+            .Where(numbers.ContainsKey)
+            .Select(x => numbers[x])
+            .Distinct();
+
+    public record Model(IReadOnlyDictionary<Position, (Position Start, int Value)> Numbers, IReadOnlyDictionary<Position, char> Symbols);
 }
