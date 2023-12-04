f3b98050e123f90427f411d7fb2c9109ee340159
Will Smith <will@toxon.co.uk>
2023-12-04 07:54:17 +0000

    2023 Day04

---
+++ AdventOfCode2023/Day04.Parser.cs
@@ -0,0 +1,47 @@
+﻿using Superpower;
+using Superpower.Parsers;
+using Superpower.Tokenizers;
+
+namespace AdventOfCode2023;
+
+public partial class Day04
+{
+    public enum TokenType
+    {
+        Card,
+        Number,
+        Colon,
+        Pipe,
+        NewLine,
+    }
+    
+    protected override Tokenizer<TokenType> Tokenizer { get; } = new TokenizerBuilder<TokenType>()
+        .Match(Character.EqualTo('\n'), TokenType.NewLine)
+        .Ignore(Span.WhiteSpace)
+        .Match(Span.EqualTo("Card"), TokenType.Card)
+        .Match(Numerics.Integer, TokenType.Number)
+        .Match(Character.EqualTo(':'), TokenType.Colon)
+        .Match(Character.EqualTo('|'), TokenType.Pipe)
+        .Build();
+
+    private static readonly TokenListParser<TokenType, int> NumberParser = 
+        Token.EqualTo(TokenType.Number)
+            .Select(x => int.Parse(x.Span.ToStringValue()));
+
+    private static readonly TokenListParser<TokenType, int> HeaderParser = 
+        Token.EqualTo(TokenType.Card)
+            .IgnoreThen(NumberParser)
+            .ThenIgnore(Token.EqualTo(TokenType.Colon));
+
+    private static readonly TokenListParser<TokenType, Card> CardParser = 
+        HeaderParser
+            .Then(NumberParser.Many())
+            .ThenIgnore(Token.EqualTo(TokenType.Pipe))
+            .Then(NumberParser.Many())
+            .Select(x => new Card(x.Item1.Item1, x.Item1.Item2, x.Item2));
+    
+    protected override TokenListParser<TokenType, Model> Parser { get; } =
+        CardParser
+            .ManyDelimitedBy(Token.EqualTo(TokenType.NewLine))
+            .Select(x => new Model(x));
+}
--- AdventOfCode2023/Day04.cs
+++ AdventOfCode2023/Day04.cs
@@ -1,12 +1,51 @@
 ﻿namespace AdventOfCode2023;
 
 [Day]
-public partial class Day04 : Day<Day04.Model, int, int>
+public partial class Day04 : ParseDay<Day04.Model, Day04.TokenType, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    private const string Sample = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample(Sample, 13)]
+    protected override int Part1(Model input)
+    {
+        var points = 0;
 
-    public record Model;
+        foreach (var card in input.Cards)
+        {
+            var count = CountMatches(card);
+            points += count == 0 ? 0 : (int)Math.Pow(2, count - 1);
+        }
+        
+        return points;
+    }
+
+    [Sample(Sample, 30)]
+    protected override int Part2(Model input)
+    {
+        var cardCounts = input.Cards.ToDictionary(x => x.Id, _ => 1);
+
+        var count = 0;
+        foreach (var card in input.Cards)
+        {
+            var cardCount = cardCounts[card.Id];
+            count += cardCount;
+            
+            var matches = CountMatches(card);
+            for (var i = 0; i < matches; i++)
+            {
+                var nextId = card.Id + i + 1;
+                if (cardCounts.TryGetValue(nextId, out var nextCardCount))
+                {
+                    cardCounts[nextId] = nextCardCount + cardCount;
+                }
+            }
+        }
+
+        return count;
+    }
+    
+    private static int CountMatches(Card card) => card.Chosen.Count(n => card.Winners.Contains(n));
+
+    public record Model(IReadOnlyList<Card> Cards);
+    public record Card(int Id, IReadOnlyList<int> Winners, IReadOnlyList<int> Chosen);
 }
