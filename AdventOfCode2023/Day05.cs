fda2ac4504810ec87f839670bcc1001d40cb11dd
Will Smith <will@toxon.co.uk>
2023-12-05 08:00:34 +0000

    Refactor 2023 Day05

--- AdventOfCode2023/Day05.cs
+++ AdventOfCode2023/Day05.cs
@@ -1,74 +1,46 @@
-﻿namespace AdventOfCode2023;
+﻿using System.Text.RegularExpressions;
+using Superpower;
+using Superpower.Parsers;
+
+namespace AdventOfCode2023;
 
 [Day]
-public partial class Day05 : Day<Day05.Model, long, long>
+public partial class Day05 : ParseDay<Day05.Model, long, long>
 {
-    protected override Model Parse(string input)
+    private const string Sample = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4";
+    
+    private static readonly TextParser<long[]> SeedsParser = Span.EqualTo("seeds: ").IgnoreThen(Numerics.IntegerInt64.ManyDelimitedBy(Character.EqualTo(' '))).ThenIgnore(Character.EqualTo('\n'));
+    private static readonly TextParser<Range> RangeParser = Numerics.IntegerInt64.AtLeastOnceDelimitedBy(Character.EqualTo(' ')).ThenIgnore(Character.EqualTo('\n').Optional()).Select(x => new Range(x[0], x[1], x[2]));
+    private static readonly TextParser<Range[]> MappingParser = Span.Regex(@"\n[a-z]+\-to\-[a-z]+ map:\n", RegexOptions.Multiline).IgnoreThen(RangeParser.Many());
+    
+    protected override TextParser<Model> Parser { get; } = SeedsParser.Then(MappingParser.AtLeastOnce()).Select(x => new Model(x.Item1, x.Item2));
+
+    [Sample(Sample, 35L)]
+    protected override long Part1(Model input) => input.Mappings.Aggregate(input.Seeds, ApplyMappings).Min();
+
+    private static IReadOnlyList<long> ApplyMappings(IReadOnlyList<long> values, IReadOnlyList<Range> mappings) => values.Select(value => ApplyMappings(value, mappings)).ToList();
+    private static long ApplyMappings(long value, IReadOnlyList<Range> mappings)
     {
-        var lines = input.Split('\n');
-
-        var seeds = lines[0].Split(' ').Skip(1).Select(long.Parse).ToArray();
-
-        var ranges = new List<IReadOnlyList<Range>>();
-        var currentRanges = new List<Range>();
-        
-        foreach (var s in lines.Skip(3))
+        foreach (var mapping in mappings)
         {
-            if (s.Length == 0) continue;
-            if (!char.IsDigit(s[0]))
-            {
-                ranges.Add(currentRanges);
-                currentRanges = new List<Range>();
-                continue;
-            }
+            if (value < mapping.Source) continue;
+            if (value > mapping.SourceLast) continue;
 
-            var parts = s.Split(' ').Select(long.Parse).ToArray();
-            var range = new Range(parts[0], parts[1], parts[2]);
-            
-            currentRanges.Add(range);
+            return value - mapping.Source + mapping.Destination;
         }
-        
-        ranges.Add(currentRanges);
 
-        return new Model(seeds, ranges);
+        return value;
     }
 
-    [Sample("seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4", 35L)]
-    protected override long Part1(Model input) => input.Ranges.Aggregate(input.Seeds, ApplyRanges).Min();
-
-    private IReadOnlyList<long> ApplyRanges(IReadOnlyList<long> values, IReadOnlyList<Range> ranges)
-    {
-        var newValues = new List<long>();
-        foreach (var value in values)
-        {
-            var found = false;
-            foreach (var range in ranges)
-            {
-                if (value < range.Source) continue;
-                if (value > range.SourceLast) continue;
-                
-                newValues.Add(value - range.Source + range.Destination);
-                found = true;
-                break;
-            }
-
-            if (!found)
-            {
-                newValues.Add(value);
-            }
-        }
-        return newValues;
-    }
-
-    [Sample("seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4", 46L)]
+    [Sample(Sample, 46L)]
     protected override long Part2(Model input)
     {
         IReadOnlyList<SeedRange> seedRanges = input.Seeds.BatchesOf2().Select(x => new SeedRange(x.A, x.B)).ToArray();
 
-        return input.Ranges.Aggregate(seedRanges, ApplyRanges).Select(x => x.Start).Min();
+        return input.Mappings.Aggregate(seedRanges, ApplyMappings).Select(x => x.Start).Min();
     }
 
-    private IReadOnlyList<SeedRange> ApplyRanges(IReadOnlyList<SeedRange> values, IReadOnlyList<Range> ranges)
+    private static IReadOnlyList<SeedRange> ApplyMappings(IReadOnlyList<SeedRange> values, IReadOnlyList<Range> mappings)
     {
         var newValues = new List<SeedRange>();
 
@@ -76,47 +48,43 @@ public partial class Day05 : Day<Day05.Model, long, long>
         while (search.Count > 0)
         {
             var value = search.Dequeue();
-            
-            var found = false;
-            foreach (var range in ranges)
-            {
-                if (value.Last < range.Source) continue;
-                if (value.Start > range.SourceLast) continue;
-
-                // split value range so that anything before the matched range is queued to retry
-                if (value.Start < range.Source)
-                {
-                    var (beforeExclusive, afterInclusive) = value.SplitAt(range.Source);
-                    search.Enqueue(beforeExclusive);
-                    value = afterInclusive;
-                }
-                
-                // split value range so that anything after the matched range is queued to retry
-                if (value.Last > range.SourceLast)
-                {
-                    var (beforeExclusive, afterInclusive) = value.SplitAt(range.SourceLast + 1);
-                    search.Enqueue(afterInclusive);
-                    value = beforeExclusive;
-                }
-
-                var newStart = value.Start - range.Source + range.Destination;
-                
-                newValues.Add(value with { Start = newStart });
-                found = true;
-                break;
-            }
-
-            if (!found)
-            {
-                newValues.Add(value);
-            }
-
+            newValues.Add(ApplyMappings(value, mappings, search));
         }
 
         return newValues;
     }
+    private static SeedRange ApplyMappings(SeedRange value, IReadOnlyList<Range> mappings, Queue<SeedRange> search)
+    {
+        foreach (var mapping in mappings)
+        {
+            if (value.Last < mapping.Source) continue;
+            if (value.Start > mapping.SourceLast) continue;
 
-    public record Model(IReadOnlyList<long> Seeds, IReadOnlyList<IReadOnlyList<Range>> Ranges);
+            // split value range so that anything before the matched range is queued to retry
+            if (value.Start < mapping.Source)
+            {
+                var (beforeExclusive, afterInclusive) = value.SplitAt(mapping.Source);
+                search.Enqueue(beforeExclusive);
+                value = afterInclusive;
+            }
+
+            // split value range so that anything after the matched range is queued to retry
+            if (value.Last > mapping.SourceLast)
+            {
+                var (beforeExclusive, afterInclusive) = value.SplitAt(mapping.SourceLast + 1);
+                search.Enqueue(afterInclusive);
+                value = beforeExclusive;
+            }
+
+            var newStart = value.Start - mapping.Source + mapping.Destination;
+
+            return value with { Start = newStart };
+        }
+
+        return value;
+    }
+
+    public record Model(IReadOnlyList<long> Seeds, IReadOnlyList<IReadOnlyList<Range>> Mappings);
 
     public record Range(long Destination, long Source, long Length)
     {
