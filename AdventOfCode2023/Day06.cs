d84950f8df3c06831786d36443598c20e1d18c9e
Will Smith <will@toxon.co.uk>
2023-12-06 07:51:47 +0000

    Optimise 2023 Day06

--- AdventOfCode2023/Day06.cs
+++ AdventOfCode2023/Day06.cs
@@ -17,7 +17,7 @@ public partial class Day06 : Day<Day06.Model, long, long>
     }
 
     [Sample("Time:      7  15   30\nDistance:  9  40  200", 288L)]
-    protected override long Part1(Model input) => input.Races.Aggregate(1, (current, race) => current * CalculateWins(race));
+    protected override long Part1(Model input) => input.Races.Aggregate(1L, (current, race) => current * CalculateWins(race));
     
     [Sample("Time:      7  15   30\nDistance:  9  40  200", 71503L)]
     protected override long Part2(Model input) =>
@@ -26,20 +26,31 @@ public partial class Day06 : Day<Day06.Model, long, long>
             long.Parse(string.Join("", input.Races.Select(x => x.DistanceRecord)))
         ));
     
-    private static int CalculateWins(Race race)
+    private static long CalculateWins(Race race)
     {
         var wins = 0;
 
-        for (var chargeTime = 0L; chargeTime < race.TimeAllow; chargeTime++)
-        {
-            var distanceTravelled = (race.TimeAllow - chargeTime) * chargeTime;
-            if (distanceTravelled > race.DistanceRecord)
-            {
-                wins++;
-            }
-        }
+        // distance = (race.TimeAllow - chargeTime) * chargeTime
+        // distance = -(chargeTime^2) + (race.TimeAllow * chargeTime)
+        // find distance > race.DistanceRecord
+        // -(chargeTime^2) + (race.TimeAllow * chargeTime) - race.DistanceRecord > 0
+        
+        // ax^2 + bx + c = 0
+        // roots = (-b +- sqrt(b^2 - 4ac)) / 2a
 
-        return wins;
+        // a = -1
+        // b = race.TimeAllow
+        // c = -race.DistanceRecord
+        
+        var sqrt = Math.Sqrt(race.TimeAllow * race.TimeAllow - 4 * race.DistanceRecord);
+        var rootA = (-race.TimeAllow + sqrt) / -2;
+        var rootB = (-race.TimeAllow - sqrt) / -2;
+
+        // if the roots are exact values then they don't **beat** the record
+        if (rootA - Math.Floor(rootA) == 0) rootA++;        
+        if (rootB - Math.Ceiling(rootB) == 0) rootB--;        
+        
+        return (long)Math.Floor(rootB) - (long)Math.Ceiling(rootA) + 1;
     }
 
     public record Model(IReadOnlyList<Race> Races);
