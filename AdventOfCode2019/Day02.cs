4651c9d23c252e67f1572ee15a3ef0a05a4ecc3e
Will Smith <will@toxon.co.uk>
2023-12-09 11:52:34 +0000

    2019 Day02

--- AdventOfCode2019/Day02.cs
+++ AdventOfCode2019/Day02.cs
@@ -3,10 +3,69 @@ namespace AdventOfCode2019;
 [Day]
 public partial class Day02 : Day<Day02.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    protected override Model Parse(string input) => new(input.Split(',').Select(int.Parse).ToArray());
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample("1,9,10,3,2,3,11,0,99,30,40,50", 3500)]
+    [Sample("1,0,0,0,99", 2)]
+    [Sample("2,5,0,0,99,3", 6)]
+    [Sample("2,4,4,0,99,0", 9801)]
+    protected override int Part1(Model input)
+    {
+        var memory = input.InitialMemory.ToArray();
 
-    public record Model;
+        // this only applies to the actual puzzle, not the samples
+        if (memory.Length > 20)
+        {
+            memory[1] = 12;
+            memory[2] = 2;
+        }
+
+        return Run(memory);
+    }
+    
+    protected override int Part2(Model input)
+    {
+        for(var noun = 0; noun < 100; noun++)
+        for (var verb = 0; verb < 100; verb++)
+        {
+            var memory = input.InitialMemory.ToArray();
+
+            memory[1] = noun;
+            memory[2] = verb;
+
+            if (Run(memory) == 19690720) return noun * 100 + verb;
+        }
+        
+        throw new Exception("no.");
+    }
+
+    private static int Run(int[] memory)
+    {
+        var ip = 0;
+        while (true)
+        {
+            switch (memory[ip])
+            {
+                case 1:
+                {
+                    memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
+                    ip += 4;
+                    break;
+                }
+                case 2:    
+                {
+                    memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
+                    ip += 4;
+                    break;
+                }
+
+                
+                case 99: return memory[0];
+                
+                default: throw new NotImplementedException($"unhandled opcode: {memory[ip]}");
+            }
+        }
+    }
+    
+    public record Model(IReadOnlyList<int> InitialMemory);
 }
