3aa06114dfe514cdf2b8ba0c1f8e67e1c1f3ef84
Will Smith <will@toxon.co.uk>
2023-12-10 13:58:18 +0000

    2019 Day08

--- AdventOfCode2019/Day08.cs
+++ AdventOfCode2019/Day08.cs
@@ -1,12 +1,38 @@
 namespace AdventOfCode2019;
 
 [Day]
-public partial class Day08 : Day<Day08.Model, int, int>
+public partial class Day08 : Day<Day08.Model, int, string>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    private const int Width = 25;
+    private const int Height = 6;
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    protected override Model Parse(string input) => new(input.BatchesOfN(Width * Height).Select(layer => new Grid<int>(layer.Select(x => x - '0').ToArray(), Width)).ToList());
 
-    public record Model;
+    protected override int Part1(Model input)
+    {
+        var minZeroLayer = input.Layers.MinBy(x => x.Count(y => y == 0));
+
+        return minZeroLayer.Count(x => x == 1) * minZeroLayer.Count(x => x == 2);
+    }
+
+    protected override string Part2(Model input)
+    {
+        var finalGrid = Grid.Empty<bool>(Width, Height);
+        
+        for (var i = input.Layers.Count - 1; i >= 0; i--)
+        {
+            var layer = input.Layers[i];
+
+            foreach (var position in layer.Keys())
+            {
+                var cell = layer[position];
+
+                if (cell != 2) finalGrid[position] = cell == 0;
+            }
+        }
+
+        return "\n" + finalGrid.Print(x => x ? ' ' : '#');
+    }
+
+    public record Model(IReadOnlyList<Grid<int>> Layers);
 }
