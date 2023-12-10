da370abad0caca3087e8b4b28be371536564bc68
Will Smith <will@toxon.co.uk>
2023-12-10 18:53:25 +0000

    2019 Day10

--- AdventOfCode.Core/Position/Position.cs
+++ AdventOfCode.Core/Position/Position.cs
@@ -8,6 +8,7 @@ public record Position(int X, int Y)
     public static Position operator -(Position a, Position b) => new(a.X - b.X, a.Y - b.Y);
     public static Position operator *(Position a, int scale) => new(a.X * scale, a.Y * scale);
     public static Position operator *(int scale, Position a) => new(scale * a.X, scale * a.Y);
+    public static Position operator /(Position a, int scale) => new(a.X / scale, a.Y / scale);
     
     public static Position operator -(Position a) => new(-a.X, -a.Y);
 }
--- AdventOfCode2019/Day10.cs
+++ AdventOfCode2019/Day10.cs
@@ -3,10 +3,83 @@ namespace AdventOfCode2019;
 [Day]
 public partial class Day10 : Day<Day10.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    protected override Model Parse(string input) => new (GridParser.ParseBool(input, '#'));
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample(".#..#\n.....\n#####\n....#\n...##", 8)]
+    [Sample("......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####", 33)]
+    protected override int Part1(Model input)
+    {
+        return SolvePart1(input).Item2.Count;
+    }
+    
+    [Sample(".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##", 802)]
+    protected override int Part2(Model input)
+    {
+        var (position, deltas) = SolvePart1(input);
 
-    public record Model;
+        var orderedDeltas = deltas
+            .Select(delta => (delta, Math.Atan2(delta.Y, delta.X) * 180 / Math.PI + 90))
+            .Select(x => x.Item2 < 0 ? (x.delta, x.Item2 + 360) : x)
+            .OrderBy(x => x.Item2)
+            .Select(x => x.delta).ToList();
+
+        var destroyed = 0;
+        
+        var offset = 0;
+        while (input.Map.Count(x => x) > 1)
+        {
+            var delta = orderedDeltas[offset++ % orderedDeltas.Count];
+            var multiple = 1;
+            while (true)
+            {
+                var target = position + delta * multiple++;
+                if (!input.Map.IsValid(target)) break;
+
+                if (!input.Map[target]) continue;
+
+                if (++destroyed == 200)
+                {
+                    return target.X * 100 + target.Y;
+                }
+                
+                input.Map[target] = false;
+                break;
+            }
+        }
+        
+        throw new Exception("no.");
+    }
+    
+    private static (Position, HashSet<Position>) SolvePart1(Model input)
+    {
+        var max = (Position.Identity, new HashSet<Position>());
+
+        foreach (var position in input.Map.Keys())
+        {
+            if (!input.Map[position]) continue;
+
+            var visible = new HashSet<Position>();
+
+            foreach (var position2 in input.Map.Keys())
+            {
+                if (position == position2) continue;
+                if (!input.Map[position2]) continue;
+
+                var delta = position2 - position;
+                var gcd = NumberExtensions.GreatestCommonDenominator(delta.X, delta.Y);
+                var reducedDelta = delta / Math.Abs(gcd);
+                
+                visible.Add(reducedDelta);
+            }
+
+            if (max.Item2.Count < visible.Count)
+            {
+                max = (position, visible);
+            }
+        }
+
+        return max;
+    }
+
+    public record Model(Grid<bool> Map);
 }
