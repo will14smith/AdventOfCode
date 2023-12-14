7cb468f6cb04d97be4bab8dd66004f3e90258ba2
Will Smith <will@toxon.co.uk>
2023-12-14 08:22:42 +0000

    Refactor 2023 Day14
    Also get runtime to <1s

--- AdventOfCode2023/Day14.cs
+++ AdventOfCode2023/Day14.cs
@@ -10,38 +10,63 @@ public partial class Day14 : Day<Day14.Model, int, int>
         'O' => Rock.Round,
         '#' => Rock.Cube,
     };
+    private static string Print(Grid<Rock> map) => map.Print(x => x switch
+    {
+        Rock.Empty => '.',
+        Rock.Round => 'O',
+        Rock.Cube => '#',
+        _ => throw new ArgumentOutOfRangeException(nameof(x), x, null)
+    });
+
 
     [Sample("O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#....", 136)]
     protected override int Part1(Model input) => WeightNorth(TiltNorth(input.Map));
+    [Sample("O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#....", 64)]
+    protected override int Part2(Model input)
+    {
+        var cycle = CycleDetection.Detect(() => input.Map, Apply, Equal);
 
-    private (Grid<Rock> Static, IReadOnlyList<Position> Round) Prepare(Grid<Rock> map)
+        const int target = 1_000_000_000;
+        var runsRequiredToMeetTargetCycle = cycle.Offset + (target - cycle.Offset) % cycle.Length;
+
+        var targetMap = input.Map;
+        for (var i = 0; i < runsRequiredToMeetTargetCycle; i++) { targetMap = Apply(targetMap); }
+
+        return WeightNorth(targetMap);
+        
+        static Grid<Rock> Apply(Grid<Rock> map) => TiltEast(TiltSouth(TiltWest(TiltNorth(map))));
+        static bool Equal(Grid<Rock> map1, Grid<Rock> map2) => Print(map1) == Print(map2);
+    }
+
+    // the ordering of Round is important, it MUST be sorted by increasing X & increasing Y
+    // this is because the Tilt methods need to process the rocks in a specific order
+    private static (Grid<Rock> Static, IReadOnlyList<Position> Round) Prepare(Grid<Rock> map)
     {
         var newMap = Grid.Empty<Rock>(map.Width, map.Height);
         var round = new List<Position>();
-        
-        foreach (var position in map.Keys())
+
+        for (var y = 0; y < map.Height; y++)
+        for (var x = 0; x < map.Width; x++)
         {
-            switch (map[position])
+            switch (map[x, y])
             {
                 case Rock.Empty: break;
-                    
-                case Rock.Round:
-                    round.Add(position);
-                    break;
-                case Rock.Cube: newMap[position] = Rock.Cube; break;
+                case Rock.Round: round.Add(new Position(x, y)); break;
+                case Rock.Cube: newMap[x, y] = Rock.Cube; break;
                     
                 default: throw new ArgumentOutOfRangeException();
             }
+
         }
         
         return (newMap, round);
     }
 
-    private Grid<Rock> TiltNorth(Grid<Rock> map)
+    private static Grid<Rock> TiltNorth(Grid<Rock> map)
     {
         var (newMap, round) = Prepare(map);
 
-        foreach (var position in round.OrderBy(x => x.Y))
+        foreach (var position in round)
         {
             var highestY = position.Y;
             for (var y2 = position.Y; y2 >= 0; y2--)
@@ -61,11 +86,11 @@ public partial class Day14 : Day<Day14.Model, int, int>
 
         return newMap;
     }
-    private Grid<Rock> TiltWest(Grid<Rock> map)
+    private static Grid<Rock> TiltWest(Grid<Rock> map)
     {
         var (newMap, round) = Prepare(map);
 
-        foreach (var position in round.OrderBy(x => x.X))
+        foreach (var position in round)
         {
             var highestX = position.X;
             for (var x2 = position.X; x2 >= 0; x2--)
@@ -86,14 +111,14 @@ public partial class Day14 : Day<Day14.Model, int, int>
         return newMap;
     }
     
-    private Grid<Rock> TiltSouth(Grid<Rock> map)
+    private static Grid<Rock> TiltSouth(Grid<Rock> map)
     {
         var (newMap, round) = Prepare(map);
 
-        foreach (var position in round.OrderByDescending(x => x.Y))
+        foreach (var position in round.Reverse())
         {
             var highestY = position.Y;
-            for (int y2 = position.Y; y2 < map.Height; y2++)
+            for (var y2 = position.Y; y2 < map.Height; y2++)
             {
                 if (newMap[position.X, y2] is Rock.Empty)
                 {
@@ -111,14 +136,14 @@ public partial class Day14 : Day<Day14.Model, int, int>
         return newMap;
     }
     
-    private Grid<Rock> TiltEast(Grid<Rock> map)
+    private static Grid<Rock> TiltEast(Grid<Rock> map)
     {
         var (newMap, round) = Prepare(map);
 
-        foreach (var position in round.OrderByDescending(x => x.X))
+        foreach (var position in round.Reverse())
         {
             var highestX = position.X;
-            for (int x2 = position.X; x2 < map.Width; x2++)
+            for (var x2 = position.X; x2 < map.Width; x2++)
             {
                 if (newMap[x2, position.Y] is Rock.Empty)
                 {
@@ -136,59 +161,8 @@ public partial class Day14 : Day<Day14.Model, int, int>
         return newMap;
     }
 
-    private int WeightNorth(Grid<Rock> map)
-    {
-        var weight = 0;
-        foreach (var position in map.Keys())
-        {
-            if (map[position] == Rock.Round)
-            {
-                weight += map.Height - position.Y;
-            }
-        }
-        return weight;
-    }
-
-    [Sample("O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#....", 64)]
-    protected override int Part2(Model input)
-    {
-        var x = CycleDetection.Detect(() => input.Map, Apply, Equal);
-
-        var target = 1_000_000_000;
-        var runs = x.Offset + (target - x.Offset) % x.Length;
-
-        var y = input.Map;
-        for (var i = 0; i < runs; i++)
-        {
-            y = Apply(y);
-            Output.WriteLine($"{i}: {WeightNorth(y)}");
-        }
-
-        return WeightNorth(y);
-    }
-
-    private Grid<Rock> Apply(Grid<Rock> arg) => TiltEast(TiltSouth(TiltWest(TiltNorth(arg))));
-
-    private bool Equal(Grid<Rock> arg1, Grid<Rock> arg2)
-    {
-        return Print(arg1) == Print(arg2);
-    }
-    private bool Equal((int Direction, Grid<Rock> Map) arg1, (int Direction, Grid<Rock> Map) arg2)
-    {
-        return arg1.Direction == arg2.Direction && Print(arg1.Map) == Print(arg2.Map);
-    }
-
-    private string Print(Grid<Rock> map)
-    {
-        return map.Print(x => x switch
-        {
-            Rock.Empty => '.',
-            Rock.Round => 'O',
-            Rock.Cube => '#',
-            _ => throw new ArgumentOutOfRangeException(nameof(x), x, null)
-        });
-    }
-
+    private static int WeightNorth(Grid<Rock> map) => map.Keys().Where(position => map[position] == Rock.Round).Sum(position => map.Height - position.Y);
+    
     public record Model(Grid<Rock> Map);
     public enum Rock
     {
