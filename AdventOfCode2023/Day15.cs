a40d1ce1f93e765ee37d09330d2d4d999c1e60d9
Will Smith <will@toxon.co.uk>
2023-12-15 07:30:55 +0000

    Refactor 2023 Day15

--- AdventOfCode2023/Day15.cs
+++ AdventOfCode2023/Day15.cs
@@ -12,8 +12,7 @@ public partial class Day15 : Day<Day15.Model, int, int>
     [Sample("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7", 145)]
     protected override int Part2(Model input)
     {
-        var boxKeys = Enumerable.Range(0, 256).Select(_ => new List<string>()).ToArray();
-        var boxValues = Enumerable.Range(0, 256).Select(_ => new List<int>()).ToArray();
+        var hashMap = new HashMap();
         
         foreach (var value in input.Values)
         {
@@ -21,38 +20,64 @@ public partial class Day15 : Day<Day15.Model, int, int>
             if (parts.Length == 2)
             {
                 var label = parts[0];
-                var box = Hash(label); 
-
                 var lens = byte.Parse(parts[1]);
 
-                var existingIndex = boxKeys[box].IndexOf(label);
-                if (existingIndex == -1)
-                {
-                    boxKeys[box].Add(label);
-                    boxValues[box].Add(lens);
-                }
-                else
-                {
-                    boxValues[box][existingIndex] = lens;
-                }
+                hashMap.Set(label, lens);
             }
             else
             {
                 var label = value[..^1];
-                var box = Hash(label); 
-
-                var existingIndex = boxKeys[box].IndexOf(label);
-                if (existingIndex == -1) continue;
                 
-                boxKeys[box].RemoveAt(existingIndex);
-                boxValues[box].RemoveAt(existingIndex);
+                hashMap.Remove(label);
             }
         }
 
-        return boxValues.SelectMany((box, boxIndex) => box.Select((lens, lensIndex) => (boxIndex + 1) * (lensIndex + 1) * lens)).Sum();
+        return hashMap.FocusingPower();
     }
 
-    private int Hash(string value) => (byte)value.Aggregate(0, (h, c) => ((h + c) * 17) & 0xFF);
+    private static int Hash(string value) => (byte)value.Aggregate(0, (h, c) => ((h + c) * 17) & 0xFF);
     
     public record Model(IReadOnlyList<string> Values);
+
+    public class HashMap
+    {
+        private readonly List<string>[] _boxLabels = Enumerable.Range(0, 256).Select(_ => new List<string>()).ToArray();
+        private readonly List<int>[] _boxLenses = Enumerable.Range(0, 256).Select(_ => new List<int>()).ToArray();
+
+        public void Set(string label, int lens)
+        {
+            var boxIndex = Hash(label);
+
+            var boxLabels = _boxLabels[boxIndex];
+            var boxLenses = _boxLenses[boxIndex];
+
+            var existingIndex = boxLabels.IndexOf(label);
+            if (existingIndex == -1)
+            {
+                boxLabels.Add(label);
+                boxLenses.Add(lens);
+            }
+            else
+            {
+                boxLenses[existingIndex] = lens;
+            }
+        }
+
+        public void Remove(string label)
+        {
+            var boxIndex = Hash(label); 
+
+            var boxLabels = _boxLabels[boxIndex];
+            
+            var existingIndex = boxLabels.IndexOf(label);
+            if (existingIndex == -1) return;
+                
+            var boxLenses = _boxLenses[boxIndex];
+            
+            boxLabels.RemoveAt(existingIndex);
+            boxLenses.RemoveAt(existingIndex);
+        }
+
+        public int FocusingPower() => _boxLenses.SelectMany((box, boxIndex) => box.Select((lens, lensIndex) => (boxIndex + 1) * (lensIndex + 1) * lens)).Sum();
+    }
 }
