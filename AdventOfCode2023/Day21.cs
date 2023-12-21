c66e467d7e3e6756f222de2f76cd0f7e14ec4ff8
Will Smith <will@fundapps.co>
2023-12-21 23:45:47 +0000

    2023 Day21
    That needed a lot of hints...

--- AdventOfCode2023/Day21.cs
+++ AdventOfCode2023/Day21.cs
@@ -1,7 +1,7 @@
 ﻿namespace AdventOfCode2023;
 
 [Day]
-public partial class Day21 : Day<Day21.Model, int, int>
+public partial class Day21 : Day<Day21.Model, int, long>
 {
     protected override Model Parse(string input)
     {
@@ -10,80 +10,73 @@ public partial class Day21 : Day<Day21.Model, int, int>
         return new Model(map);
     }
 
-    [Sample("...........\n.....###.#.\n.###.##..#.\n..#.#...#..\n....#.#....\n.##..S####.\n.##..#...#.\n.......##..\n.##.#.####.\n.##..##.##.\n...........", 0)]
-    protected override int Part1(Model input)
-    {
-        var rocks = input.Map.Keys().Where(x => input.Map[x] == '#').ToHashSet();
-        var start = input.Map.Keys().First(x => input.Map[x] == 'S');
-        
-        var locations = new HashSet<Position> { start };
+    [Sample("...........\n.....###.#.\n.###.##..#.\n..#.#...#..\n....#.#....\n.##..S####.\n.##..#...#.\n.......##..\n.##.#.####.\n.##..##.##.\n...........", 2460)]
+    protected override int Part1(Model input) => FindDistance(input).Count(x => (x.Value % 2) == 0 && x.Value < 65);
 
-        for (var i = 0; i < 50; i++)
+    protected override long Part2(Model input)
+    {
+        var steps = 26501365L;
+        var (full, part) = Math.DivRem(steps, input.Map.Width);
+
+        if ((full & 1) != 0 || part != input.Map.Width / 2)
         {
-            locations = locations.SelectMany(x => x.OrthogonalNeighbours()).ToHashSet();
-            locations.ExceptWith(rocks);
+            throw new Exception("this ain't gonna work");
         }
+        
+        // at 0 full + part we have 1 odd - 1 odd corners and 0 even
+        // at 1 full + part we have 1 odd + 1 odd corners and 4 even - 2 even corners
+        // at 2 full + part we have 9 odd - 3 odd corners and 4 even + 2 even corners
+        // at 3 full + part we have 9 odd + 3 odd corners and 16 even - 4 even corners
+        // at 4 full + part we have 25 odd - 5 odd corners and 16 even + 4 even corners
+        
+        // since we know full is even, we'll just code with that assumption
+        var fullOdd = (full + 1) * (full + 1);
+        var fullEven = full * full;
+        var cornersOdd = -(full + 1);
+        var cornersEven = full;
 
-        return locations.Count;
+        var distances = FindDistance(input);
+        
+        var oddFullCount = distances.Count(x => (x.Value & 1) == 1);
+        var evenFullCount = distances.Count(x => (x.Value & 1) == 0);
+        
+        var oddCornerCount = distances.Count(x => (x.Value & 1) == 1 && x.Value > part);
+        // I cannot explain the minus 1, but it is needed.
+        var evenCornerCount = distances.Count(x => (x.Value & 1) == 0 && x.Value > part) - 1;
+
+        return fullOdd * oddFullCount + fullEven * evenFullCount + cornersOdd * oddCornerCount + cornersEven * evenCornerCount;
     }
 
-    [Sample("...........\n.....###.#.\n.###.##..#.\n..#.#...#..\n....#.#....\n.##..S####.\n.##..#...#.\n.......##..\n.##.#.####.\n.##..##.##.\n...........", 0)]
-    protected override int Part2(Model input)
-    {        
+    private IReadOnlyDictionary<Position, int> FindDistance(Model input)
+    {
         var rocks = input.Map.Keys().Where(x => input.Map[x] == '#').ToHashSet();
         var start = input.Map.Keys().First(x => input.Map[x] == 'S');
 
-        var cycleStart = FindSteadyState(input, rocks, start);
-        Output.WriteLine(cycleStart.ToString());
-        
-        var cycleBottomMiddle = FindSteadyState(input, rocks, new Position((input.Map.Width - 1) / 2, input.Map.Height - 1));
-        Output.WriteLine(cycleBottomMiddle.ToString());
-        var cycleTopMiddle = FindSteadyState(input, rocks, new Position((input.Map.Width - 1) / 2, 0));
-        Output.WriteLine(cycleTopMiddle.ToString());
-        var cycleLeftMiddle = FindSteadyState(input, rocks, new Position(0, (input.Map.Height - 1) / 2));
-        Output.WriteLine(cycleLeftMiddle.ToString());
-        var cycleRightMiddle = FindSteadyState(input, rocks, new Position(input.Map.Width - 1, (input.Map.Height - 1) / 2));
-        Output.WriteLine(cycleRightMiddle.ToString());
+        var locations = new Dictionary<Position, int>();
 
-        var cycleTopLeft = FindSteadyState(input, rocks, new Position(0, 0));
-        Output.WriteLine(cycleTopLeft.ToString());
-        var cycleTopRight = FindSteadyState(input, rocks, new Position(input.Map.Width - 1,0 ));
-        Output.WriteLine(cycleTopRight.ToString());
-        var cycleBottomLeft = FindSteadyState(input, rocks, new Position(0, input.Map.Height - 1));
-        Output.WriteLine(cycleBottomLeft.ToString());
-        var cycleBottomRight = FindSteadyState(input, rocks, new Position(input.Map.Width - 1, input.Map.Height - 1));
-        Output.WriteLine(cycleBottomRight.ToString());
+        var search = new Queue<(Position, int)>();
+        search.Enqueue((start, 0));
 
-        // after 66 cycle: start 4 new grids
-        // after 132 cycles
-        
-        return 0;
-    }
-
-    private (int Cycle, int Even, int Odd) FindSteadyState(Model input, IReadOnlySet<Position> rocks, Position start)
-    {
-        var locations = new HashSet<Position> { start };
-
-        var prevCounts = (-1, -1); 
-        
-        for (var i = 1; i < 500; i++)
+        while (search.Count > 0)
         {
-            locations = locations.SelectMany(x => x.OrthogonalNeighbours()).Where(x => input.Map.IsValid(x)).ToHashSet();
-            locations.ExceptWith(rocks);
-
-            if (locations.Count == prevCounts.Item1)
+            var (position, distance) = search.Dequeue();
+            if (!locations.TryAdd(position, distance))
             {
-                var (even, odd) = prevCounts;
-                if (i % 2 == 1) (even, odd) = (odd, even);
-                
-                return (i - 2, even, odd);
+                continue;
             }
             
-            prevCounts = (prevCounts.Item2, locations.Count);
+            foreach (var neighbour in position.OrthogonalNeighbours())
+            {
+                if (!input.Map.IsValid(neighbour)) continue;
+                if (rocks.Contains(neighbour)) continue;
+                if (locations.ContainsKey(neighbour)) continue;
+                
+                search.Enqueue((neighbour, distance + 1));
+            }
         }
 
-        throw new Exception("no");
+        return locations;
     }
-    
+
     public record Model(Grid<char> Map);
 }
