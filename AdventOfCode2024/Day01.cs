2af2268db4fb1557147cdeec027ca56066bd4d18
Will Smith <will@toxon.co.uk>
2024-12-01 09:12:24 +0000

    Solve Day1

--- AdventOfCode2024/Day01.cs
+++ AdventOfCode2024/Day01.cs
@@ -3,10 +3,17 @@ namespace AdventOfCode2024;
 [Day]
 public partial class Day01 : Day<Day01.Model, int, int>
 {
-    public record Model;
+    public record Model(IReadOnlyList<int> Left, IReadOnlyList<int> Right);
     
-    protected override Model Parse(string input) => throw new NotImplementedException();
-    
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    protected override Model Parse(string input)
+    {
+        var lines = input.Split('\n').Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)).ToArray();
+        
+        return new Model(lines.Select(x => int.Parse(x[0])).ToArray(), lines.Select(x => int.Parse(x[1])).ToArray());
+    }
+
+    [Sample("3   4\n4   3\n2   5\n1   3\n3   9\n3   3", 11)]
+    protected override int Part1(Model input) => input.Left.OrderBy(x => x).Zip(input.Right.OrderBy(x =>x)).Sum(x => Math.Abs(x.First - x.Second));
+    [Sample("3   4\n4   3\n2   5\n1   3\n3   9\n3   3", 31)]
+    protected override int Part2(Model input) => input.Left.Sum(x => x * input.Right.Count(y => y == x));
 }
