50b5bd9b20a9a19a89bf62e5f77f14d9c8777948
Will Smith <will@toxon.co.uk>
2024-12-06 19:23:27 +0000

    Refactor Day6
    I would say I optimised it but by `AsParallel` is a LOT of the heavy lifting

--- AdventOfCode2024/Day06.cs
+++ AdventOfCode2024/Day06.cs
@@ -3,7 +3,7 @@ namespace AdventOfCode2024;
 [Day]
 public partial class Day06 : Day<Day06.Model, int, int>
 {
-    public record Model(Grid<Cell> Grid);
+    public record Model(Grid<Cell> Grid, Position Start);
     public enum Cell
     {
         Empty,
@@ -11,34 +11,29 @@ public partial class Day06 : Day<Day06.Model, int, int>
         Obstacle
     }
     
-    protected override Model Parse(string input) => new Model(GridParser.ParseChar(input, x => x switch
+    protected override Model Parse(string input)
     {
-        '.' => Cell.Empty,
-        '^' => Cell.Start,
-        '#' => Cell.Obstacle,
-    }));
-    
-    [Sample("....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...", 41)]
-    protected override int Part1(Model input)
-    {
-        var visited = GetVisited(input);
-
-        return visited.Count;
-    }
-    
-    [Sample("....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...", 6)]
-    protected override int Part2(Model input)
-    {
-        var where = GetVisited(input).Where(x => IsLoop(input, x)).ToList();
+        var grid = GridParser.ParseChar(input, x => x switch
+        {
+            '.' => Cell.Empty,
+            '^' => Cell.Start,
+            '#' => Cell.Obstacle,
+        });
         
-        return GetVisited(input).Count(extra => IsLoop(input, extra));
+        return new Model(grid, grid.Keys().First(x => grid[x] == Cell.Start));
     }
+
+    [Sample("....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...", 41)]
+    protected override int Part1(Model input) => GetVisited(input).Count;
+
+    [Sample("....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...", 6)]
+    protected override int Part2(Model input) => GetVisited(input).Where(x => x != input.Start).AsParallel().Count(extra => IsLoop(input, extra));
 
     private static HashSet<Position> GetVisited(Model input)
     {
         var visited = new HashSet<Position>();
         
-        var position = input.Grid.Keys().First(x => input.Grid[x] == Cell.Start);
+        var position = input.Start;
         var heading = new Position(0, -1);
         
         while (input.Grid.IsValid(position))
@@ -58,12 +53,11 @@ public partial class Day06 : Day<Day06.Model, int, int>
         return visited;
     }
 
-    private bool IsLoop(Model input, Position extraObstacle)
+    private static bool IsLoop(Model input, Position extraObstacle)
     {
         var visitedStates = new HashSet<(Position Position, Position Heading)>();
         
-        var position = input.Grid.Keys().First(x => input.Grid[x] == Cell.Start);
-        if (position == extraObstacle) return false; // invalid
+        var position = input.Start;
         var heading = new Position(0, -1);
         
         while (input.Grid.IsValid(position))
