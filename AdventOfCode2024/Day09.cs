4e28df62124d639fa90cfedef0613d60519b97d6
Will Smith <will@toxon.co.uk>
2024-12-10 23:08:49 +0000

    Optimise Day9
    Using a linked list makes merging adjacent free blocks much easier
    avoiding a full compaction each time

--- AdventOfCode2024/Day09.cs
+++ AdventOfCode2024/Day09.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace AdventOfCode2024;
 
 [Day]
@@ -10,27 +12,27 @@ public partial class Day09 : Day<Day09.Model, long, long>
     protected override long Part1(Model input)
     {
         var disk = new List<DiskEntry>();
+        var files = new Stack<DiskEntry>();
         for (var i = 0; i < input.Map.Count; i++)
         {
             if (i % 2 == 0)
             {
-                disk.Add(new DiskEntry.Allocated(i / 2, input.Map[i]));
+                var file = DiskEntry.Allocated(i / 2, input.Map[i]);
+                disk.Add(file);
+                files.Push(file);
             }
             else
             {
-                disk.Add(new DiskEntry.Free(input.Map[i]));
+                disk.Add(DiskEntry.Free(input.Map[i]));
             }
         }
         
-        var files = new Stack<DiskEntry.Allocated>();
-        disk.OfType<DiskEntry.Allocated>().ToList().ForEach(f => files.Push(f));
-
-        var newDisk = new List<DiskEntry.Allocated>();
+        var newDisk = new List<DiskEntry>();
         foreach (var entry in disk)
         {
-            if (entry is DiskEntry.Free free)
+            if (entry.IsFree)
             {
-                var remaining = free.Size;
+                var remaining = entry.Size;
 
                 while (remaining > 0)
                 {
@@ -42,21 +44,21 @@ public partial class Day09 : Day<Day09.Model, long, long>
                     }
                     else if (top.Size > remaining)
                     {
-                        newDisk.Add(new DiskEntry.Allocated(top.Id, remaining));
-                        files.Push(new DiskEntry.Allocated(top.Id, top.Size - remaining));
+                        newDisk.Add(DiskEntry.Allocated(top.Id!.Value, remaining));
+                        files.Push(DiskEntry.Allocated(top.Id.Value, top.Size - remaining));
                         remaining = 0;
                     }
                 }
             } 
-            else if (entry is DiskEntry.Allocated allocated)
+            else
             {
-                if (allocated.Id == files.Peek().Id)
+                if (entry.Id == files.Peek().Id)
                 {
                     newDisk.Add(files.Pop());
                     break;
                 }
                 
-                newDisk.Add(allocated);
+                newDisk.Add(entry);
             }
         }
 
@@ -66,91 +68,100 @@ public partial class Day09 : Day<Day09.Model, long, long>
     [Sample("2333133121414131402", 2858L)]
     protected override long Part2(Model input)
     {
-        var disk = new List<DiskEntry>();
+        var disk = new LinkedList<DiskEntry>();
+        var files = new Stack<LinkedListNode<DiskEntry>>();
+
         for (var i = 0; i < input.Map.Count; i++)
         {
+            var size = input.Map[i];
             if (i % 2 == 0)
             {
-                disk.Add(new DiskEntry.Allocated(i / 2, input.Map[i]));
+                files.Push(disk.AddLast(DiskEntry.Allocated(i / 2, size)));
             }
-            else
+            else if(size > 0)
             {
-                disk.Add(new DiskEntry.Free(input.Map[i]));
+                disk.AddLast(DiskEntry.Free(size));
             }
         }
-
-        var files = new Stack<DiskEntry.Allocated>();
-        disk.OfType<DiskEntry.Allocated>().ToList().ForEach(f => files.Push(f));
-
-        foreach (var file in files)
+        
+        foreach (var fileEntry in files)
         {
-            var findIndex = disk.FindIndex(x => x is DiskEntry.Free free && free.Size >= file.Size);
-            var fileIndex = disk.IndexOf(file);
+            var file = fileEntry.Value;
 
-            if (findIndex >= 0 && findIndex < fileIndex)
+            var freeEntry = disk.First;
+            while (freeEntry != null)
             {
-                disk[fileIndex] = new DiskEntry.Free(file.Size);
-                
-                var free = (DiskEntry.Free)disk[findIndex];
-                disk.RemoveAt(findIndex);
-
-                if (free.Size > file.Size)
+                if (freeEntry == fileEntry)
                 {
-                    disk.Insert(findIndex, new DiskEntry.Free(free.Size - file.Size));
+                    break;
+                }
+                
+                if (freeEntry.Value.IsFree && freeEntry.Value.Size >= file.Size)
+                {
+                    break;
+                }
+                
+                freeEntry = freeEntry.Next;
+            }
+
+            // TODO check 
+            if (freeEntry != null && freeEntry != fileEntry)
+            {
+                if (fileEntry.Previous?.Value.IsFree ?? false)
+                {
+                    if (fileEntry.Next?.Value.IsFree ?? false)
+                    {
+                        fileEntry.Previous.Value = DiskEntry.Free(fileEntry.Previous.Value.Size + file.Size + fileEntry.Next.Value.Size);
+                        disk.Remove(fileEntry.Next);
+                        disk.Remove(fileEntry);
+                    }
+                    else
+                    {
+                        fileEntry.Previous.Value = DiskEntry.Free(fileEntry.Previous.Value.Size + file.Size);
+                        disk.Remove(fileEntry);
+                    }
+                }
+                else
+                {
+                    if (fileEntry.Next?.Value.IsFree ?? false)
+                    {
+                        fileEntry.Next.Value = DiskEntry.Free(file.Size + fileEntry.Next.Value.Size);
+                        disk.Remove(fileEntry);
+                    }
+                    else
+                    {
+                        fileEntry.Value = DiskEntry.Free(file.Size);
+                    }
                 }
 
-                disk.Insert(findIndex, file);
-
-                disk = Compact(disk);
+                var free = freeEntry.Value;
+                if (free.Size > file.Size)
+                {
+                    disk.AddBefore(freeEntry, file);
+                    freeEntry.Value = DiskEntry.Free(free.Size - file.Size);
+                }
+                else
+                {
+                    freeEntry.Value = file;
+                }
             }
         }
         
         return CalculateChecksum(disk);
     }
-
-    private List<DiskEntry> Compact(IReadOnlyList<DiskEntry> previousDisk)
-    {
-        var disk = new List<DiskEntry>(previousDisk.Count);
-
-        DiskEntry? previous = null;
-        foreach (var diskEntry in previousDisk)
-        {
-            switch (diskEntry)
-            {
-                case DiskEntry.Allocated: 
-                    disk.Add(previous = diskEntry); 
-                    break;
-                    
-                case DiskEntry.Free free:
-                    if (previous is DiskEntry.Free previousFree)
-                    {
-                        disk[^1] = previous = new DiskEntry.Free(previousFree.Size + free.Size);
-                    }
-                    else
-                    {
-                        disk.Add(previous = free);
-                    }
-                    break;
-                
-                default: throw new ArgumentOutOfRangeException(nameof(diskEntry));
-            }
-        }
-
-        return disk;
-    }
-
-    private static long CalculateChecksum(IReadOnlyList<DiskEntry> disk)
+    
+    private static long CalculateChecksum(IEnumerable<DiskEntry> disk)
     {
         var checksum = 0L;
         var idx = 0L;
         
         foreach (var entry in disk)
         {
-            if (entry is DiskEntry.Allocated allocated)
+            if (!entry.IsFree)
             {
                 for (var i = 0; i < entry.Size; i++)
                 {
-                    checksum += allocated.Id * idx++;
+                    checksum += entry.Id.Value * idx++;
                 }
             }
             else
@@ -162,15 +173,14 @@ public partial class Day09 : Day<Day09.Model, long, long>
         return checksum;
     }
     
-    private abstract class DiskEntry(long size)
+    private class DiskEntry(long? id, long size)
     {
+        public long? Id { get; } = id;
         public long Size { get; } = size;
-
-        public class Allocated(long Id, long size) : DiskEntry(size)
-        {
-            public long Id { get; } = Id;
-        }
-
-        public class Free(long size) : DiskEntry(size);
+        [MemberNotNullWhen(false, nameof(Id))]
+        public bool IsFree { get; } = id is null;
+        
+        public static DiskEntry Allocated(long id, long size) => new(id, size);
+        public static DiskEntry Free(long size) => new(null, size);
     }
 }
