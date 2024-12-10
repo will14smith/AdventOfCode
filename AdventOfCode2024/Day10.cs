e3012e9d0755e06678637919ed019786c868ccec
Will Smith <will@toxon.co.uk>
2024-12-10 22:41:07 +0000

    Solve Day10

--- AdventOfCode2024/Day10.cs
+++ AdventOfCode2024/Day10.cs
@@ -3,10 +3,68 @@ namespace AdventOfCode2024;
 [Day]
 public partial class Day10 : Day<Day10.Model, int, int>
 {
-    public record Model;
+    public record Model(Grid<int> Map);
+    protected override Model Parse(string input) => new(GridParser.ParseInt(input));
     
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    [Sample("89010123\n78121874\n87430965\n96549874\n45678903\n32019012\n01329801\n10456732", 36)]
+    protected override int Part1(Model input) => input.Map.Keys().Where(key => input.Map[key] == 0).Sum(position => CountReachable(input.Map, position));
+
+    private static int CountReachable(Grid<int> map, Position start)
+    {
+        var queue = new Queue<Position>();
+        queue.Enqueue(start);
+
+        var visited = new HashSet<Position>();
+        
+        while (queue.Count > 0)
+        {
+            var position  = queue.Dequeue();
+            if (map[position] == 9)
+            {
+                visited.Add(position);
+                continue;
+            }
+            
+            foreach (var neighbour in position.OrthogonalNeighbours().Where(map.IsValid))
+            {
+                if (map[neighbour] == map[position] + 1)
+                {
+                    queue.Enqueue(neighbour);
+                }
+            }
+        }
+
+        return visited.Count;
+    }
     
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample("89010123\n78121874\n87430965\n96549874\n45678903\n32019012\n01329801\n10456732", 81)]
+    protected override int Part2(Model input) => input.Map.Keys().Where(key => input.Map[key] == 0).Sum(position => CountRoutes(input.Map, position));
+
+    private static int CountRoutes(Grid<int> map, Position start)
+    {
+        var queue = new Queue<Position>();
+        queue.Enqueue(start);
+
+        var visited = new List<Position>();
+        
+        while (queue.Count > 0)
+        {
+            var position = queue.Dequeue();
+            if (map[position] == 9)
+            {
+                visited.Add(position);
+                continue;
+            }
+            
+            foreach (var neighbour in position.OrthogonalNeighbours().Where(map.IsValid))
+            {
+                if (map[neighbour] == map[position] + 1)
+                {
+                    queue.Enqueue(neighbour);
+                }
+            }
+        }
+
+        return visited.Count;
+    }
 }
