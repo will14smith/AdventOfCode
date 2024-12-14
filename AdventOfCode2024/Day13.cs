2787e40344b9d161f11d40b4953f2609f4b52b50
Will Smith <will@toxon.co.uk>
2024-12-14 14:47:41 +0000

    Refactor Day13
    Actually solve the equations

--- AdventOfCode2024/Day13.cs
+++ AdventOfCode2024/Day13.cs
@@ -16,41 +16,43 @@ public partial class Day13 : ParseDay<Day13.Model, long, long>
     protected override TextParser<Model> Parser { get; } = MachineParser.ManyDelimitedBy(Span.EqualTo("\n\n")).Select(m => new Model(m));
 
     [Sample("Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400\n\nButton A: X+26, Y+66\nButton B: X+67, Y+21\nPrize: X=12748, Y=12176\n\nButton A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450\n\nButton A: X+69, Y+23\nButton B: X+27, Y+71\nPrize: X=18641, Y=10279", 480L)]
-    protected override long Part1(Model input) => input.Machines.Sum(SolveCheapest);
+    protected override long Part1(Model input) => input.Machines.Sum(x => SolveCheapest(x, 0));
+    protected override long Part2(Model input) => input.Machines.Sum(x => SolveCheapest(x, 10000000000000));
 
-    private long SolveCheapest(Machine machine)
+    private long SolveCheapest(Machine machine, long offset)
     {
-        var ctx = new Context();
-        var optimize = ctx.MkOptimize();
-
-        var a = ctx.MkIntConst("a");
-        var b = ctx.MkIntConst("b");
-
-        optimize.Assert(a >= 0);
-        optimize.Assert(b >= 0);
-        optimize.Assert(ctx.MkEq(machine.A.X * a + machine.B.X * b, ctx.MkInt(machine.Prize.X)));
-        optimize.Assert(ctx.MkEq(machine.A.Y * a + machine.B.Y * b, ctx.MkInt(machine.Prize.Y)));
-        var min = optimize.MkMinimize(3 * a + b);
+        // a*Ax + b*Bx = Px
+        // a*Ay + b*By = Py
         
-        return optimize.Check() == Status.SATISFIABLE ? ((IntNum)min.Value).Int64 : 0;
-    }
-
-    protected override long Part2(Model input) => input.Machines.Sum(SolveCheapest2);
-    
-    private long SolveCheapest2(Machine machine)
-    {
-        var ctx = new Context();
-        var optimize = ctx.MkOptimize();
-
-        var a = ctx.MkIntConst("a");
-        var b = ctx.MkIntConst("b");
-
-        optimize.Assert(a >= 0);
-        optimize.Assert(b >= 0);
-        optimize.Assert(ctx.MkEq(machine.A.X * a + machine.B.X * b, ctx.MkInt(machine.Prize.X + 10000000000000)));
-        optimize.Assert(ctx.MkEq(machine.A.Y * a + machine.B.Y * b, ctx.MkInt(machine.Prize.Y + 10000000000000)));
-        var min = optimize.MkMinimize(3 * a + b);
+        // By(a*Ax + b*Bx) = By*Px
+        // Bx(a*Ay + b*By) = Bx*Py
         
-        return optimize.Check() == Status.SATISFIABLE ? ((IntNum)min.Value).Int64 : 0;
+        // a*Ax*By + b*Bx*By = By*Px
+        // a*Ay*Bx + b*Bx*By = Bx*Py
+
+        // a*Ax*By + b*Bx*By - (a*Ay*Bx + b*Bx*By) = By*Px - Bx*Py
+        // a*Ax*By - a*Ay*Bx = By*Px - Bx*Py
+        // a * (Ax*By - Ay*Bx) = By*Px - Bx*Py
+        // a = (By*Px - Bx*Py) / (Ax*By - Ay*Bx)
+
+        var prizeX = machine.Prize.X + offset;
+        var prizeY = machine.Prize.Y + offset;
+        
+        var aNumerator = (long)machine.B.Y * prizeX - machine.B.X * prizeY;
+        var aDenominator = (long)machine.A.X * machine.B.Y - machine.A.Y * machine.B.X;
+
+        if (aNumerator % aDenominator != 0)
+        {
+            return 0;
+        }
+        
+        var a = aNumerator / aDenominator;
+        
+        // a*Ax + b*Bx = Px
+        // b*Bx = Px - a*Ax
+        // b = (Px - a*Ax) / Bx
+        var b = (prizeX - a * machine.A.X) / machine.B.X;
+
+        return a * 3 + b;
     }
 }
