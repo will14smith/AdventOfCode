c12d0d1644869c0dd77ba812fb30019e1c8394e0
Will Smith <will@toxon.co.uk>
2024-12-22 18:50:12 +0000

    Solve Day22

--- AdventOfCode2024/Day22.cs
+++ AdventOfCode2024/Day22.cs
@@ -1,12 +1,91 @@
 namespace AdventOfCode2024;
 
 [Day]
-public partial class Day22 : Day<Day22.Model, int, int>
+public partial class Day22 : Day<Day22.Model, long, long>
 {
-    public record Model;
+    public record Model(IReadOnlyList<long> Buyers);
     
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    protected override Model Parse(string input) => new(input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray());
     
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    [Sample("1\n10\n100\n2024\n", 37327623L)]
+    protected override long Part1(Model input)
+    {
+        return input.Buyers.Sum(x => NextN(x, 2000));
+    }
+
+    private long NextN(long secret, int n)
+    {
+        for (var i = 0; i < n; i++)
+        {
+            secret = Next(secret);
+        }
+
+        return secret;
+    }
+    
+    [Sample("1\n2\n3\n2024\n", 23L)]
+    protected override long Part2(Model input)
+    {
+        var ngrams = new Dictionary<string, long>();
+        foreach (var buyer in input.Buyers)
+        {
+            var seenForBuyer = new HashSet<string>();
+            
+            var a = buyer;
+            var b = Next(a);
+            var c = Next(b);
+            var d = Next(c);
+
+            for (var i = 0; i < 2000 - 2; i++)
+            {
+                var e = Next(d);
+
+                var price = e % 10;
+
+                var diff1 = b % 10 - a % 10;
+                var diff2 = c % 10 - b % 10;
+                var diff3 = d % 10 - c % 10;
+                var diff4 = e % 10 - d % 10;
+
+                a = b;
+                b = c;
+                c = d;
+                d = e;
+                
+                var ngram = $"{diff1},{diff2},{diff3},{diff4}";
+                if (!seenForBuyer.Add(ngram))
+                {
+                    continue;
+                }
+                
+                if (ngrams.TryGetValue(ngram, out var existing))
+                {
+                    ngrams[ngram] = existing + price;
+                }
+                else
+                {
+                    ngrams[ngram] = price;
+                }
+            }
+        }
+        
+        return ngrams.Max(x => x.Value);
+    }
+    
+    private static long Next(long secret)
+    {
+        var result = secret * 64;
+        secret ^= result;
+        secret &= 0xFFFFFF;
+
+        result = secret / 32;
+        secret ^= result;
+        secret &= 0xFFFFFF;
+
+        result = secret * 2048;
+        secret ^= result;
+        secret &= 0xFFFFFF;
+
+        return secret;
+    }
 }
