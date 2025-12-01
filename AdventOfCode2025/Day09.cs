59fb03461627ccb9b59dd971691659c8b02c3b01
Will Smith <will@toxon.co.uk>
2025-12-01 22:05:48 +0000

    Setup 2025

---
+++ AdventOfCode2025/Day09.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode2025;
+
+[Day]
+public partial class Day09 : Day<Day09.Model, int, int>
+{
+    public record Model;
+
+    protected override Model Parse(string input) => new();
+
+    [Sample("", 1)]
+    protected override int Part1(Model input) => 0;
+    [Sample("", 1)]
+    protected override int Part2(Model input) => 0;
+}
