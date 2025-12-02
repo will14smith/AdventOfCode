ec789072da308b9087f3cc70599663b7befcc11c
Will Smith <will@toxon.co.uk>
2025-12-02 19:28:13 +0000

    Solve 2018 Day1

--- AdventOfCode2018/Day01.cs
+++ AdventOfCode2018/Day01.cs
@@ -3,10 +3,28 @@ namespace AdventOfCode2018;
 [Day]
 public partial class Day01 : Day<Day01.Model, int, int>
 {
-    protected override Model Parse(string input) => throw new NotImplementedException();
+    public record Model(IReadOnlyList<int> Frequencies);
 
-    protected override int Part1(Model input) => throw new NotImplementedException();
-    protected override int Part2(Model input) => throw new NotImplementedException();
+    protected override Model Parse(string input) => new(input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
 
-    public record Model;
+    [Sample("+1\n-2\n+3\n+1\n", 3)]
+    protected override int Part1(Model input) => input.Frequencies.Sum();
+    [Sample("+1\n-2\n+3\n+1\n", 2)]
+    protected override int Part2(Model input)
+    {
+        var frequency = 0;
+        var frequencies = new HashSet<int> { 0 };
+        
+        while (true)
+        {
+            foreach (var change in input.Frequencies)
+            {
+                frequency += change;
+                if (!frequencies.Add(frequency))
+                {
+                    return frequency;
+                }
+            }
+        }
+    }
 }
